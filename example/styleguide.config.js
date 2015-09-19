1f40f974c372819be5053246f162b39408d7059a
Artem Sapegin <artem@sapegin.ru>
2015-09-19 10:05:32 +0200

    Example: move components to components folder.

--- example/styleguide.config.js
+++ example/styleguide.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	title: 'Style guide example',
 	rootDir: '.',
-	components: './**/*.js',
+	components: './components/**/*.js',
 	getExampleFilename: function(componentpath) {
 		return componentpath.replace(/\.js$/, '.examples.md');
 	},
