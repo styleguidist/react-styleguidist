e1d64e5bade3aca8682f423fb9e25f6f67ff2885
Artem Sapegin <artem@sapegin.ru>
2015-09-20 16:19:25 +0200

    Fix Markdown rendering.

--- loaders/examples.loader.js
+++ loaders/examples.loader.js
@@ -5,47 +5,35 @@ var config = require('../src/utils/config');
 
 var defaultRenderer = marked.Renderer.prototype;
 
-var MARKED_BLOCK_METHODS = [
-	'blockquote',
-	'html',
-	'heading',
-	'hr',
-	'list',
-	'paragraph',
-	'table'
-];
-
 function readExamples(markdown) {
-	var chunks = [];
-	var htmlChunks = [];
+	var codePlaceholder = '<%{#}%>';
+	var codeChunks = [];
 
 	var renderer = new marked.Renderer();
-	wrapRendererMethods(renderer, defaultRenderer, MARKED_BLOCK_METHODS, function(html) {
-		htmlChunks.push(html);
-	});
+	renderer.heading = function(text, level, raw) {
+		var tag = 'h' + (level + 2);
+		return '<' + tag + '>' + text + '</' + tag + '>\n';
+	};
 	renderer.code = function(code) {
-		if (htmlChunks.length) {
-			chunks.push({type: 'html', content: htmlChunks.join('')});
-			htmlChunks = [];
-		}
-		chunks.push({type: 'code', content: code});
+		codeChunks.push(code);
+		return codePlaceholder;
 	};
 
-	marked(markdown, {renderer: renderer});
+	var html = marked(markdown, {renderer: renderer});
 
-	if (htmlChunks.length) {
-		chunks.push({type: 'html', content: htmlChunks.join('')});
-	}
+	var chunks = [];
+	var textChunks = html.split(codePlaceholder);
+	textChunks.forEach(function(chunk) {
+		if (chunk) {
+			chunks.push({type: 'html', content: chunk});
+		}
+		var code = codeChunks.shift();
+		if (code) {
+			chunks.push({type: 'code', content: code});
+		}
+	});
 
 	return chunks;
-}
-
-function wrapRendererMethods(to, from, methods, wrapper) {
-	methods.forEach(function(name) {
-		to[name] = function() {
-			wrapper(from[name].apply(from, arguments));
-		};
-	});
 }
 
 module.exports = function (source, map) {
