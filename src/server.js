bbf611b87b808fbbeb04a8e2d88c4f8ea0715431
Artem Sapegin <artem@sapegin.ru>
2015-09-20 16:27:48 +0200

    Make server more reusable.

--- src/server.js
+++ src/server.js
@@ -3,20 +3,17 @@ var webpack = require('webpack');
 var makeWebpackConfig = require('./make-webpack-config');
 var config = require('./utils/config');
 
-var app = express();
-var compiler = webpack(makeWebpackConfig('development'));
+module.exports = function server(callback) {
+	var app = express();
+	var compiler = webpack(makeWebpackConfig('development'));
 
-app.use(require('webpack-dev-middleware')(compiler, {
-	noInfo: true
-}));
+	app.use(require('webpack-dev-middleware')(compiler, {
+		noInfo: true
+	}));
 
-app.use(require('webpack-hot-middleware')(compiler));
+	app.use(require('webpack-hot-middleware')(compiler));
 
-app.listen(config.serverPort, config.serverHost, function (err) {
-	if (err) {
-		console.log(err);
-		return;
-	}
-
-	console.log('Listening at http://' + config.serverHost + ':' + config.serverPort);
-});
+	app.listen(config.serverPort, config.serverHost, function (err) {
+		callback(err, config);
+	});
+}
