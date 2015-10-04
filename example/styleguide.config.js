d4eda533cb486c9d9359498358b693ce1a538619
Artem Sapegin <artem@sapegin.ru>
2015-10-04 09:46:58 +0200

    Fix example.

--- example/styleguide.config.js
+++ example/styleguide.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	title: 'Style guide example',
-	rootDir: '.',
+	rootDir: './lib',
 	components: function(config, glob) {
 		return glob.sync(config.rootDir + '/components/**/*.js').filter(function(module) {
 			return /\/[A-Z][a-z]*\.js$/.test(module);
