0fbd4f015b2e5a863f0acf0f83e474fa90c40af3
Artem Sapegin <artem@sapegin.ru>
2015-10-14 13:48:46 +0200

    Better error handling when react-docgen cannot parse component source.
    Log error to the terminal, do not stop style guide generation.

--- loaders/props.loader.js
+++ loaders/props.loader.js
@@ -1,9 +1,19 @@
+var path = require('path');
 var reactDocs = require('react-docgen');
 
-module.exports = function (source, map) {
+module.exports = function (source) {
 	this.cacheable && this.cacheable();
 
-	var props = reactDocs.parse(source);
+    var props;
+    try {
+	   props = reactDocs.parse(source);
+    }
+    catch (e) {
+        console.log('Error when parsing', path.basename(this.request));
+        console.log(e.toString());
+        console.log();
+        props = {};
+    }
 
 	return [
 			'if (module.hot) {',
