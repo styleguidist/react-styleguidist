38e2d3db67ea071668a8343180800b32f8812772
Artem Sapegin <artem@sapegin.ru>
2015-10-28 11:28:56 +0100

    Move layout to the new Layout component, StyleGuide component is just a container.

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,27 @@
+import { Component, PropTypes } from 'react';
+import Components from 'components/Components';
+
+import s from './Layout.css';
+
+export default class Layout extends Component {
+	static propTypes = {
+		config: PropTypes.object.isRequired,
+		components: PropTypes.array.isRequired
+	}
+
+	// TODO: Blank slate
+
+	render() {
+		let { config, components } = this.props;
+		let { title, highlightTheme } = config;
+
+		return (
+			<div className={s.root}>
+				<h1 className={s.heading}>{title}</h1>
+				<div>
+					<Components highlightTheme={highlightTheme} components={components}/>
+				</div>
+			</div>
+		);
+	}
+}
--- src/components/StyleGuide/StyleGuide.js
+++ src/components/StyleGuide/StyleGuide.js
@@ -1,27 +1,10 @@
 import { Component, PropTypes } from 'react';
-import Components from 'components/Components';
-
-import s from './StyleGuide.css';
+import Layout from 'components/Layout';
 
 export default class StyleGuide extends Component {
-	static propTypes = {
-		config: PropTypes.object.isRequired,
-		components: PropTypes.array.isRequired
-	}
-
-	// TODO: Blank slate
-
 	render() {
-		let { config, components } = this.props;
-		let { title, highlightTheme } = config;
-
 		return (
-			<div className={s.root}>
-				<h1 className={s.heading}>{title}</h1>
-				<div>
-					<Components highlightTheme={highlightTheme} components={components}/>
-				</div>
-			</div>
+			<Layout {...this.props}/>
 		);
 	}
 }
