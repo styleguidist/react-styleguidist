056b160298ac9eeae8674d563ce19de72af8ac97
Artem Sapegin <artem@sapegin.ru>
2015-11-09 22:41:53 +0100

    Add babel-plugin-react-display-name. Should preserve displayName when using React.createComponent. Fix #18.

--- src/make-webpack-config.js
+++ src/make-webpack-config.js
@@ -17,8 +17,10 @@ module.exports = function(env) {
 	}
 
 	var reactTransformPath = path.join(__dirname, '../../babel-plugin-react-transform');  // npm 3
+	var reactDisplayNamePath = path.join(__dirname, '../../babel-plugin-react-display-name');  // npm 3
 	if (!fs.existsSync(reactTransformPath)) {
 		reactTransformPath = path.resolve(__dirname, '../node_modules/babel-plugin-react-transform');  // npm 2 or react-styleguidist develop
+		reactDisplayNamePath = path.resolve(__dirname, '../node_modules/babel-plugin-react-display-name');  // npm 2 or react-styleguidist develop
 	}
 
 	var includes = [
@@ -146,7 +148,10 @@ module.exports = function(env) {
 						loader: 'babel',
 						query: {
 							stage: 0,
-							plugins: [reactTransformPath],
+							plugins: [
+								reactDisplayNamePath,
+								reactTransformPath
+							],
 							extra: {
 								'react-transform': {
 									transforms: [
