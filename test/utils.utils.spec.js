2935eca6da4d829e9a0bfe5056d3f5ec6d60133a
Artem Sapegin <artem@sapegin.ru>
2015-11-09 22:33:20 +0100

    Add Mocha and Chai.

---
+++ test/utils.utils.spec.js
@@ -0,0 +1,48 @@
+import { expect } from 'chai';
+import _ from 'lodash';
+
+import * as utils from '../src/utils/utils';
+
+describe('utils', () => {
+
+	describe('setComponentsNames', () => {
+		it('should set name property to each component', () => {
+			let result = utils.setComponentsNames([
+				{
+					module: {displayName: 'Foo'}
+				},
+				{
+					module: {name: 'Bar'}
+				}
+			]);
+			expect(_.pluck(result, 'name')).to.eql(['Foo', 'Bar']);
+		});
+	});
+
+	describe('globalizeComponents', () => {
+		let sourceGlobalLength;
+		beforeEach(() => {
+			sourceGlobalLength = Object.keys(global).length;
+		});
+		afterEach(() => {
+			delete global.Foo;
+			delete global.Bar;
+		});
+		it('should set each component’s module as a global variable', () => {
+			utils.globalizeComponents([
+				{
+					name: 'Foo',
+					module: 13
+				},
+				{
+					name: 'Bar',
+					module: 27
+				}
+			]);
+			expect(Object.keys(global).length).to.eql(sourceGlobalLength + 2);
+			expect(global.Foo).to.eql(13);
+			expect(global.Bar).to.eql(27);
+		});
+	});
+
+});
