0c5802fecbf88aafebf716b6edc914a4a5c3b2d6
Artem Sapegin <artem@sapegin.ru>
2015-11-16 19:53:46 +0100

    Add very basic tests for Props component.

---
+++ test/components.props.spec.js
@@ -0,0 +1,57 @@
+import { parse } from 'react-docgen';
+
+import Props from '../src/components/Props';
+
+function render(propTypes) {
+	let props = parse(`
+		import { Component, PropTypes } from 'react';
+		export default class Cmpnt extends Component {
+			static propTypes = {
+				${propTypes.join(',')}
+			}
+			render() {
+			}
+		}
+	`);
+	return <Props props={props}/>;
+}
+
+describe('Props', () => {
+
+	it('should render PropTypes.string', () => {
+		let result = render(['color: PropTypes.string']);
+		expectReactShallow(result).to.contain(
+			<tr>
+				<td><code>color</code></td>
+				<td><code>string</code></td>
+				<td><span>Optional</span></td>
+				<td><div/></td>
+			</tr>
+		);
+	});
+
+	it('should render PropTypes.string.isRequired', () => {
+		let result = render(['color: PropTypes.string.isRequired']);
+		expectReactShallow(result).to.contain(
+			<tr>
+				<td><code>color</code></td>
+				<td><code>string</code></td>
+				<td></td>
+				<td><div/></td>
+			</tr>
+		);
+	});
+
+	it('should render PropTypes.arrayOf', () => {
+		let result = render(['colors: PropTypes.arrayOf(PropTypes.string)']);
+		expectReactShallow(result).to.contain(
+			<tr>
+				<td><code>colors</code></td>
+				<td><code>string[]</code></td>
+				<td><span>Optional</span></td>
+				<td><div/></td>
+			</tr>
+		);
+	});
+
+});
