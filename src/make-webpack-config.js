596c42d7cde87fb02fda48417d6f244bec7d4406
Artem Sapegin <artem@sapegin.ru>
2015-11-18 20:15:26 +0100

    Remove babel-plugin-react-display-name.
    It should be unnecessary because it’s a dependency of both babel-core
    (for Babel 5) and babel-preset-react (for Babel 6). Fix #18.

--- src/make-webpack-config.js
+++ src/make-webpack-config.js
@@ -18,10 +18,8 @@ module.exports = function(env) {
 	}
 
 	var reactTransformPath = path.join(__dirname, '../../babel-plugin-react-transform');  // npm 3
-	var reactDisplayNamePath = path.join(__dirname, '../../babel-plugin-react-display-name');  // npm 3
 	if (!fs.existsSync(reactTransformPath)) {
 		reactTransformPath = path.resolve(__dirname, '../node_modules/babel-plugin-react-transform');  // npm 2 or react-styleguidist develop
-		reactDisplayNamePath = path.resolve(__dirname, '../node_modules/babel-plugin-react-display-name');  // npm 2 or react-styleguidist develop
 	}
 
 	var includes = [
@@ -154,7 +152,6 @@ module.exports = function(env) {
 						query: {
 							stage: 0,
 							plugins: [
-								reactDisplayNamePath,
 								reactTransformPath
 							],
 							extra: {
