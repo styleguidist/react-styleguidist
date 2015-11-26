4a27cb89dc45e68b0d78f01e4e78716cd31fdc05
Artem Sapegin <artem@sapegin.ru>
2015-11-26 22:54:00 +0100

    Refactor Button example as a new React stateless component.

--- example/lib/components/Button/Button.js
+++ example/lib/components/Button/Button.js
@@ -5,33 +5,33 @@ import s from './Button.css';
 /**
  * The only true button.
  */
-export default class Button extends Component {
-	static propTypes = {
-		/**
-		 * Button label.
-		 */
-		children: PropTypes.string.isRequired,
-		color: PropTypes.string,
-		size: PropTypes.oneOf(['small', 'normal', 'large']),
-	}
-	static defaultProps = {
-		color: '#333',
-		size: 'normal'
-	}
-	static sizes = {
-		small: '10px',
-		normal: '14px',
-		large: '18px'
-	}
+const Button = ({ color, size, children}) => {
+	let styles = {
+		color: color,
+		fontSize: Button.sizes[size]
+	};
 
-	render() {
-		let styles = {
-			color: this.props.color,
-			fontSize: Button.sizes[this.props.size]
-		};
+	return (
+		<button className={s.root} style={styles}>{children}</button>
+	);
 
-		return (
-			<button className={s.root} style={styles}>{this.props.children}</button>
-		);
-	}
-}
+};
+Button.propTypes = {
+	/**
+	 * Button label.
+	 */
+	children: PropTypes.string.isRequired,
+	color: PropTypes.string,
+	size: PropTypes.oneOf(['small', 'normal', 'large']),
+};
+Button.defaultProps = {
+	color: '#333',
+	size: 'normal'
+};
+Button.sizes = {
+	small: '10px',
+	normal: '14px',
+	large: '18px'
+};
+
+export default Button;
