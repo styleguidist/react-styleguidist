cc667e9113d5f11fea41c188d97bfd9563a556fd
Artem Sapegin <artem@sapegin.ru>
2015-11-30 08:28:04 +0100

    Reformat code.

--- loaders/props.loader.js
+++ loaders/props.loader.js
@@ -4,21 +4,21 @@ var reactDocs = require('react-docgen');
 module.exports = function (source) {
 	this.cacheable && this.cacheable();
 
-    var props;
-    try {
-	   props = reactDocs.parse(source);
-    }
-    catch (e) {
-        console.log('Error when parsing', path.basename(this.request));
-        console.log(e.toString());
-        console.log();
-        props = {};
-    }
+	var props;
+	try {
+		props = reactDocs.parse(source);
+	}
+	catch (e) {
+		console.log('Error when parsing', path.basename(this.request));
+		console.log(e.toString());
+		console.log();
+		props = {};
+	}
 
 	return [
-			'if (module.hot) {',
-			'	module.hot.accept([]);',
-			'}',
-			'module.exports = ' + JSON.stringify(props)
-		].join('\n');
+		'if (module.hot) {',
+		'	module.hot.accept([]);',
+		'}',
+		'module.exports = ' + JSON.stringify(props)
+	].join('\n');
 };
