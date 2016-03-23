dbaf7a183c104a782ad6522b236402c61e06fd49
Paul Jones <pauljones23@gmail.com>
2016-03-23 11:18:22 +1000

    Hooks to support multiple components being declared per file

--- loaders/props.loader.js
+++ loaders/props.loader.js
@@ -1,12 +1,13 @@
 var path = require('path');
 var reactDocs = require('react-docgen');
+var config = require('../src/utils/config');
 
 module.exports = function (source) {
 	this.cacheable && this.cacheable();
 
 	var props;
 	try {
-		props = reactDocs.parse(source);
+		props = reactDocs.parse(source, config.resolver, config.handlers);
 	}
 	catch (e) {
 		console.log('Error when parsing', path.basename(this.request));
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { setComponentsNames, globalizeComponents } from './utils/utils';
 import StyleGuide from 'rsg-components/StyleGuide';
+import _ from 'lodash';
 
 import 'highlight.js/styles/tomorrow.css';
 import './styles.css';
@@ -14,6 +15,15 @@ if (module.hot) {
 
 // Load style guide
 let { config, components } = require('styleguide!');
+
+// If any of the components have multiple children, flatten them.
+components = _.flatMap(components, c => {
+  if (_.isArray(c.props)) {
+    return c.props.map(props => _.extend({}, c, {props: props}))
+  } else {
+    return c;
+  }
+});
 
 components = setComponentsNames(components);
 globalizeComponents(components);
