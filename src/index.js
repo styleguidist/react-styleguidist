b406c7cfd16c1054d2e3d3ccb054990f2d1679e1
Karl Brightman <karl.brightman@envato.com>
2016-04-11 14:25:58 +0800

    Fix linting issues and duplicate check for components and sections

--- src/index.js
+++ src/index.js
@@ -35,13 +35,12 @@ function processSections(ss) {
 
 // Components are required unless sections are provided
 if (sections) {
-	sections = processSections(sections)
-	components = []
-} else {
-	components = processComponents(components)
-	sections = []
+	sections = processSections(sections);
+	components = [];
 }
-components = components ? processComponents(components) : [];
-sections = sections ? processSections(sections) : [];
+else {
+	components = processComponents(components);
+	sections = [];
+}
 
 ReactDOM.render(<StyleGuide config={config} components={components} sections={sections} />, document.getElementById('app'));
