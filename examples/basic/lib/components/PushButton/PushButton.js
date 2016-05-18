45f8f9d007069de0fb2c1c1a63a5169d4fb6de92
Jeremy Gayed <jeremy.gayed@nytimes.com>
2016-05-18 17:38:24 -0400

    Introduce use case (example-less component)

---
+++ examples/basic/lib/components/PushButton/PushButton.js
@@ -0,0 +1,38 @@
+import { Component, PropTypes } from 'react';
+
+import s from './PushButton.css';
+
+/**
+ * An example-less button.
+ */
+export default function PushButton({
+	color,
+	size,
+	children
+}) {
+	let styles = {
+		color: color,
+		fontSize: PushButton.sizes[size]
+	};
+
+	return (
+		<button className={s.root} style={styles}>{children}</button>
+	);
+}
+PushButton.propTypes = {
+	/**
+	 * PushButton label.
+	 */
+	children: PropTypes.string.isRequired,
+	color: PropTypes.string,
+	size: PropTypes.oneOf(['small', 'normal', 'large']),
+};
+PushButton.defaultProps = {
+	color: '#333',
+	size: 'normal'
+};
+PushButton.sizes = {
+	small: '10px',
+	normal: '14px',
+	large: '18px'
+};
