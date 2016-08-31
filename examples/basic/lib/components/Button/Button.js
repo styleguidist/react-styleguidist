13aa3761b0fbac308173de4b2880457a8606ad1e
Andres Gutierrez <andres@redbooth.com>
2016-08-31 13:24:44 +0200

    Reproduce undefined error on documented method

--- examples/basic/lib/components/Button/Button.js
+++ examples/basic/lib/components/Button/Button.js
@@ -1,38 +1,55 @@
-import { Component, PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 import s from './Button.css';
 
-/**
- * The only true button.
- */
-export default function Button({
-	color,
-	size,
-	children
-}) {
-	let styles = {
-		color: color,
-		fontSize: Button.sizes[size]
-	};
-
-	return (
-		<button className={s.root} style={styles}>{children}</button>
-	);
-}
-Button.propTypes = {
-	/**
-	 * Button label.
-	 */
-	children: PropTypes.string.isRequired,
-	color: PropTypes.string,
-	size: PropTypes.oneOf(['small', 'normal', 'large']),
-};
-Button.defaultProps = {
-	color: '#333',
-	size: 'normal'
-};
-Button.sizes = {
+const ButtonSizes = {
 	small: '10px',
 	normal: '14px',
 	large: '18px'
 };
+
+/**
+ * The only true button.
+ */
+const Button = React.createClass({
+  propTypes: {
+	  /**
+	   * Button label.
+	   */
+	  children: PropTypes.string.isRequired,
+	  color: PropTypes.string,
+	  size: PropTypes.oneOf(['small', 'normal', 'large']),
+  },
+
+  getDefaultProps() {
+    return {
+	    color: '#333',
+	    size: 'normal'
+    };
+  },
+
+  /**
+   * Documentation of method
+   */
+  onClick() {
+  },
+
+  render() {
+    const {
+      color,
+      size,
+      children
+    } = this.props;
+
+    let styles = {
+      color: color,
+      fontSize: Button.sizes[size]
+    };
+
+    return (
+      <button onClick={this.onClick} className={s.root} style={styles}>{children}</button>
+    );
+  },
+});
+
+export default Button;
