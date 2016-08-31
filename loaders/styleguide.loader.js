85e3aa014d9c446518a0bc627a49c3592f8abbf6
Artem Sapegin <artem@sapegin.ru>
2016-08-31 21:23:07 +0200

    Prettify style gude loader, add super stupid test.

--- loaders/styleguide.loader.js
+++ loaders/styleguide.loader.js
@@ -1,27 +1,28 @@
-var fs = require('fs');
-var path = require('path');
-var glob = require('glob');
-var prettyjson = require('prettyjson');
-var _ = require('lodash');
-var config = require('../src/utils/config');
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+const prettyjson = require('prettyjson');
+const pick = require('lodash/pick');
+
+const config = require('../src/utils/config');
 
 function processComponent(filepath) {
     // If component name can’t be detected in runtime use filename or folder name (if file name is 'index')
-	var filename = path.basename(filepath).replace(/\.\w+$/, '');
-	var nameFallbak = filename === 'index' ? path.basename(path.dirname(filepath)) : filename;
+	const filename = path.basename(filepath).replace(/\.\w+$/, '');
+	const nameFallback = filename === 'index' ? path.basename(path.dirname(filepath)) : filename;
 
 	return '{' + [
 		'filepath: ' + JSON.stringify(filepath),
-		'nameFallbak: ' + JSON.stringify(nameFallbak),
+		'nameFallbak: ' + JSON.stringify(nameFallback),
 		'pathLine: ' + JSON.stringify(config.getComponentPathLine(path.relative(config.configDir, filepath))),
 		'module: ' + requireIt(filepath),
 		'props: ' + requireIt('!!props!' + filepath),
-		'examples: ' + getExamples(filepath, nameFallbak)
+		'examples: ' + getExamples(filepath, nameFallback),
 	].join(',') + '}';
 }
 
 function getExamples(filepath, nameFallbak) {
-	var examplesFile = config.getExampleFilename(filepath);
+	const examplesFile = config.getExampleFilename(filepath);
 
 	if (hasExamples(filepath)) {
 		return requireIt('examples!' + examplesFile);
@@ -35,7 +36,7 @@ function getExamples(filepath, nameFallbak) {
 }
 
 function hasExamples(filepath) {
-	var examplesFile = config.getExampleFilename(filepath);
+	const examplesFile = config.getExampleFilename(filepath);
 	return !!fs.existsSync(examplesFile);
 }
 
@@ -44,9 +45,11 @@ function requireIt(filepath) {
 }
 
 function processComponentsSource(components, config) {
-	if (!components) return null;
+	if (!components) {
+		return null;
+	}
 
-	var componentSources;
+	let componentSources;
 	if (typeof components === 'function') {
 		componentSources = components();
 	}
@@ -62,7 +65,7 @@ function processComponentsSource(components, config) {
 	}
 
 	if (config.skipComponentsWithoutExample) {
-		componentSources = _.filter(componentSources, hasExamples);
+		componentSources = componentSources.filter(hasExamples);
 	}
 
 	return '[' + componentSources.map(processComponent).join(',') + ']';
@@ -73,42 +76,46 @@ function processSection(section, config) {
 		'name: ' + JSON.stringify(section.name),
 		'content: ' + (section.content ? requireIt('examples!' + path.resolve(config.configDir, section.content)) : null),
 		'components: ' + processComponentsSource(section.components, config),
-		'sections: ' + processSectionsList(section.sections, config)
+		'sections: ' + processSectionsList(section.sections, config),
 	].join(',') + '}';
 }
 
 function processSectionsList(sections, config) {
-	if (!sections) return null;
+	if (!sections) {
+		return null;
+	}
 
 	return '[' +
-		sections.map(function(section) { return processSection(section, config); }).join(',') +
+		sections.map(section => processSection(section, config)).join(',') +
 	']';
 }
 
 module.exports = function() {};
 module.exports.pitch = function() {
-	this.cacheable && this.cacheable();
+	if (this.cacheable) {
+		this.cacheable();
+	}
 
 	// Example usage of query options:
 	// require('!!styleguide?{"configFilepath":"/path/to/config.js"}!');
 	if (this.query) {
-		var query = JSON.parse(this.query.substring(1)); // getting rid of '?'
+		const query = JSON.parse(this.query.substring(1));  // getting rid of '?'
 		if (query.configFilepath) {
 			config.initialize(query.configFilepath);
 		}
 	}
 
-	var simplifiedConfig = _.pick(config, [
+	const simplifiedConfig = pick(config, [
 		'title',
 		'highlightTheme',
-		'showCode'
+		'showCode',
 	]);
 
-	return [
-		'module.exports = {',
-		'	config: ' + JSON.stringify(simplifiedConfig) + ',',
-		'	components: ' + processComponentsSource(config.components, config) + ',',
-		'	sections: ' + processSectionsList(config.sections, config),
-		'};'
-	].join('\n');
+	return `
+		module.exports = {
+			config: ${JSON.stringify(simplifiedConfig)},
+			components: ${processComponentsSource(config.components, config)},
+			sections: ${processSectionsList(config.sections, config)}
+		};
+	`;
 };
---
+++ test/loaders.styleguide.spec.js
@@ -0,0 +1,10 @@
+import test from 'ava';
+import { readFileSync } from 'fs';
+import styleguideLoader from '../loaders/styleguide.loader';
+
+test('should return valid, parsable JS', t => {
+	const file = '../examples/basic/lib/components/Button/Button.js';
+	const result = styleguideLoader.pitch.call({ request: file }, readFileSync(file, 'utf8'));
+	t.truthy(result);
+	t.notThrows(() => new Function(result), SyntaxError);  // eslint-disable-line no-new-func
+});
