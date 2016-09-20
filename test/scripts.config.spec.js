20678f1152878a4d4a0c562a159d2f2f16826d18
Artem Sapegin <artem@sapegin.ru>
2016-09-20 20:58:29 +0200

    One more test.

--- test/scripts.config.spec.js
+++ test/scripts.config.spec.js
@@ -1,9 +1,14 @@
 import test from 'ava';
 import getConfig from '../scripts/config';
 
-test('should find a config file', t => {
+test('should read a config file', t => {
 	const result = getConfig({ config: 'data/styleguide.config.js' });
-
 	t.truthy(result);
 	t.is(result.title, 'React Style Guide Example');
 });
+
+test('should throw when config file not found', t => {
+	const fn = () => getConfig({});
+	t.throws(fn);
+});
+
