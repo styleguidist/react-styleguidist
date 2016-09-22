016983c921e712757640709ddebcba291d20c63e
Zachary Halvorson <zacharyhalvorson@me.com>
2016-09-22 14:49:16 -0700

    removed other footer

--- src/rsg-components/Layout/Renderer.js
+++ src/rsg-components/Layout/Renderer.js
@@ -3,9 +3,6 @@ import React, { PropTypes } from 'react';
 const Renderer = ({ title, components }) => (
 	<main>
 		{components}
-		<footer>
-			Generated with <a href="https://github.com/sapegin/react-styleguidist">React Styleguidist</a>
-		</footer>
 	</main>
 );
 
