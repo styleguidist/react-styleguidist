da3e142601cb7de1775ce635a4755f4116b2606f
Zachary Halvorson <zacharyhalvorson@me.com>
2016-10-06 14:19:17 -0700

    hover effects

--- src/rsg-components/Components/Components.js
+++ src/rsg-components/Components/Components.js
@@ -70,7 +70,7 @@ export default class Components extends Component {
                     <div className="mw8 center ph2">
                         <input
                             ref="search-input"
-                            className="db w-100 ph1 h2_5 bw1 br1 b--solid b--black-20 lh-input"
+                            className="db w-100 ph1 h2_5 bw1 br1 b--solid b--black-20 lh-input trans-all shadow-hover"
                             placeholder="What Are You Looking For?"
                             onChange={this.onSearchTermChange}
                             value={searchTerm}
