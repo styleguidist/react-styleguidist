styleguidist/react-styleguidist

commit 28f233184ee42b7ca50e5bba30724e7d700382c4

Add react-docgen-displayname-handler (#205)
Improves support of HOCs.

Fixes #202.

scripts/config.js
@@ -11,6 +11,7 @@ const merge = require('lodash/merge');
 const utils = require('./utils/utils');
 const consts = require('./consts');
 const StyleguidistError = require('./utils/error');
+const displayNameHandler = require('react-docgen-displayname-handler').default;
 
 const CONFIG_FILENAME = 'styleguide.config.js';
 const DEFAULT_CONFIG = {
@@ -30,6 +31,9 @@ const DEFAULT_CONFIG = {
 	getExampleFilename: componentpath => path.join(path.dirname(componentpath), 'Readme.md'),
 	getComponentPathLine: componentpath => componentpath,
 	updateWebpackConfig: null,
+	handlers: [
+		displayNameHandler,
+	],
 };
 const DEPENDENCIES = [
 	{

examples/basic/lib/components/WrappedButton/WrappedButton.css
@@ -1,0 +1,10 @@
+.root {
+	padding: .5em 1.5em;
+	color: #666;
+	background-color: #fff;
+	border: 1px solid currentColor;
+	border-radius: .3em;
+	text-align: center;
+	vertical-align: middle;
+	cursor: pointer;
+}

examples/basic/lib/components/WrappedButton/WrappedButton.js
@@ -1,0 +1,47 @@
+import React, { Component, PropTypes } from 'react';
+import s from './WrappedButton.css';
+
+
+/**
+ * A button wrapped by a Decorator/Enhancer
+ */
+const WrappedButton = ({
+	color,
+	size,
+	children,
+}) => {
+	let styles = {
+		color,
+		fontSize: WrappedButton.sizes[size],
+	};
+
+	return (
+		<button className={s.root} style={styles}>{children}</button>
+	);
+};
+WrappedButton.propTypes = {
+	/**
+	 * Button label.
+	 */
+	children: PropTypes.string.isRequired,
+	color: PropTypes.string,
+	size: PropTypes.oneOf(['small', 'normal', 'large']),
+};
+WrappedButton.defaultProps = {
+	color: '#333',
+	size: 'normal',
+};
+WrappedButton.sizes = {
+	small: '10px',
+	normal: '14px',
+	large: '18px',
+};
+
+
+const Decorator = Composed => class MyHOC extends Component {
+	render() {
+		return <Composed {...this.props} />;
+	}
+};
+
+export default Decorator(WrappedButton);

examples/basic/lib/components/WrappedButton/index.js
@@ -1,0 +1,1 @@
+export { default } from './WrappedButton.js';

commit 8b1b846eb77927c10b81a7d7c35a29a718e246b4

Fix regression introduced in #205: missed props tables.
We still need react-docgen default handlers, not just displayName
handler :ambulance:

scripts/config.js
@@ -11,6 +11,7 @@ const merge = require('lodash/merge');
 const utils = require('./utils/utils');
 const consts = require('./consts');
 const StyleguidistError = require('./utils/error');
+const reactDocgen = require('react-docgen');
 const displayNameHandler = require('react-docgen-displayname-handler').default;
 
 const CONFIG_FILENAME = 'styleguide.config.js';
@@ -31,9 +32,7 @@ const DEFAULT_CONFIG = {
 	getExampleFilename: componentpath => path.join(path.dirname(componentpath), 'Readme.md'),
 	getComponentPathLine: componentpath => componentpath,
 	updateWebpackConfig: null,
-	handlers: [
-		displayNameHandler,
-	],
+	handlers: reactDocgen.defaultHandlers.concat(displayNameHandler),
 };
 const DEPENDENCIES = [
 	{