ce8054583b7bf302cfe49a6eab1eee7b832420ea
Matteo Ronchi <matteo.ronchi@gmail.com>
2016-11-14 23:24:35 +0100

    Fix: PreviewDelay was validated also when not defined

--- scripts/config.js
+++ scripts/config.js
@@ -9,6 +9,7 @@ const semverUtils = require('semver-utils');
 const prettyjson = require('prettyjson');
 const merge = require('lodash/merge');
 const isFinite = require('lodash/isFinite');
+const isUndefined = require('lodash/isUndefined');
 const utils = require('./utils/utils');
 const consts = require('./consts');
 const StyleguidistError = require('./utils/error');
@@ -172,7 +173,7 @@ function validateConfig(config) {
 		throw new StyleguidistError('Styleguidist: "defaultExample" option must be either false, true, ' +
 			'or a string path to a markdown file.');
 	}
-	if (!isFinite(config.previewDelay)) {
+	if (!isUndefined(config.previewDelay) && !isFinite(config.previewDelay)) {
 		throw new StyleguidistError('Styleguidist: "previewDelay" option must be a positive number.');
 	}
 }
