6385d5725ba78265d1926a458f37423c16c2596a
Matteo Ronchi <matteo.ronchi@gmail.com>
2016-11-14 11:10:23 +0100

    Chore: cherry-picked lodash deps and added missing comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
+import isFinite from 'lodash/isFinite';
 import ReactDOM from 'react-dom';
 import {
 	getComponentNameFromHash,
@@ -28,7 +29,15 @@ function renderStyleguide() {
 	let sidebar = true;
 	let singleExample = false;
 
-	const { targetComponentName, targetComponentIndex } = getComponentNameFromHash();
+	// parse url hash to check if the components list must be filtered
+	const {
+		// name of the filtered component to show isolated
+		targetComponentName,
+		// index of the fenced block example of the filtered component isolate
+		targetComponentIndex,
+	} = getComponentNameFromHash();
+
+	// filter the requested component id required
 	if (targetComponentName) {
 		components = [
 			...filterComponentsByExactName(components, targetComponentName),
@@ -37,7 +46,8 @@ function renderStyleguide() {
 		sections = [];
 		sidebar = false;
 
-		if (components.length === 1 && _.isFinite(targetComponentIndex)) {
+		// if a single component is filtered and a fenced block index is specified hide the other examples
+		if (components.length === 1 && isFinite(targetComponentIndex)) {
 			components[0] = filterComponentExamples(components[0], targetComponentIndex);
 			singleExample = true;
 		}
