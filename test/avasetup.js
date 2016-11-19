9346ad9e22d190214d065b3897fef07994689e33
Artem Sapegin <artem@sapegin.ru>
2016-11-19 19:39:31 +0100

    Chore: Remove css-modules-require-hook.

--- test/avasetup.js
+++ test/avasetup.js
@@ -5,7 +5,6 @@ import { shallow, render } from 'enzyme';
 import unexpected from 'unexpected';
 import unexpectedReact from 'unexpected-react';
 import mock from 'mock-require';
-import cssModulesHook from 'css-modules-require-hook';
 
 /**
  * Enzyme and unexpected-react.
@@ -24,11 +23,3 @@ require('module').Module._initPaths();
  * Mock Editor component because CodeMirror doesn’t work in Node.
  */
 mock('rsg-components/Editor', {});
-
-/**
- * CSS modules.
- */
-cssModulesHook({
-	extensions: ['.css'],
-	generateScopedName: 'Test__[local]',
-});
