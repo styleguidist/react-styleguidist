3310fdd8fa3e5c68ac8ef393fe63e9668c0cac75
Artem Sapegin <artem@sapegin.ru>
2016-12-19 22:01:10 +0100

    Docs: Fix customised example

--- examples/customised/styleguide/components/ReactComponent/index.jsx
+++ examples/customised/styleguide/components/ReactComponent/index.jsx
@@ -1,4 +1,4 @@
-import { Component, PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 const s = require('./ReactComponent.css');
 
--- examples/customised/styleguide/components/StyleGuide/index.jsx
+++ examples/customised/styleguide/components/StyleGuide/index.jsx
@@ -1,4 +1,4 @@
-import { PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 
 import s from './StyleGuide.css';
 
