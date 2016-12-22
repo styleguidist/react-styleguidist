48c718205237bc493110a208bb7785b0c95561b2
Artem Sapegin <artem@sapegin.ru>
2016-12-22 16:32:56 +0100

    Test: Add more debug info

--- scripts/utils/__tests__/findUserWebpackConfig.spec.js
+++ scripts/utils/__tests__/findUserWebpackConfig.spec.js
@@ -7,23 +7,23 @@ afterEach(() => {
 
 it('should return path to create-react-app Webpack config', () => {
 	process.chdir('test/apps/cra');
-	console.error('CWD1: ' + process.cwd());  // eslint-disable-line no-console
 	const result = findUserWebpackConfig();
+	console.error('CWD1: ' + result + '\nCWD1: ' + process.cwd());  // eslint-disable-line no-console
 	expect(result).toMatch(/^react-scripts\//);
 });
 
 it('should return an absolute path to user Webpack config located in project root folder', () => {
 	process.chdir('test/apps/basic');
-	console.error('CWD2: ' + process.cwd());  // eslint-disable-line no-console
 	const result = findUserWebpackConfig();
+	console.error('CWD2: ' + result + '\nCWD2: ' + process.cwd());  // eslint-disable-line no-console
 	expect(result).toMatch(/^\//);
 	expect(result).toMatch(/webpack.config.js$/);
 });
 
 it('should return an absolute path to user Webpack config located in nested folder', () => {
 	process.chdir('test/apps/webpack-nested');
-	console.error('CWD3: ' + process.cwd());  // eslint-disable-line no-console
 	const result = findUserWebpackConfig();
+	console.error('CWD3: ' + result + '\nCWD3: ' + process.cwd());  // eslint-disable-line no-console
 	expect(result).toMatch(/^\//);
 	expect(result).toMatch(/webpack.config.js$/);
 });
