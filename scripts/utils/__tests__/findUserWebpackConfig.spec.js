99c4163915913883c4c559e7b1d07750ca61820f
Artem Sapegin <artem@sapegin.ru>
2016-12-22 16:49:51 +0100

    Test: Remove debug info

--- scripts/utils/__tests__/findUserWebpackConfig.spec.js
+++ scripts/utils/__tests__/findUserWebpackConfig.spec.js
@@ -8,14 +8,12 @@ afterEach(() => {
 it('should return path to create-react-app Webpack config', () => {
 	process.chdir('test/apps/cra');
 	const result = findUserWebpackConfig();
-	console.error('CWD1: ' + result + '\nCWD1: ' + process.cwd());  // eslint-disable-line no-console
 	expect(result).toMatch(/^react-scripts\//);
 });
 
 it('should return an absolute path to user Webpack config located in project root folder', () => {
 	process.chdir('test/apps/basic');
 	const result = findUserWebpackConfig();
-	console.error('CWD2: ' + result + '\nCWD2: ' + process.cwd());  // eslint-disable-line no-console
 	expect(result).toMatch(/^\//);
 	expect(result).toMatch(/webpack.config.js$/);
 });
@@ -23,7 +21,6 @@ it('should return an absolute path to user Webpack config located in project roo
 it('should return an absolute path to user Webpack config located in nested folder', () => {
 	process.chdir('test/apps/webpack-nested');
 	const result = findUserWebpackConfig();
-	console.error('CWD3: ' + result + '\nCWD3: ' + process.cwd());  // eslint-disable-line no-console
 	expect(result).toMatch(/^\//);
 	expect(result).toMatch(/webpack.config.js$/);
 });
