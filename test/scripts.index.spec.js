89ed7d1621b56b591ade6405ca5307d92025f16f
Matteo Ronchi <matteo.ronchi@gmail.com>
2016-12-29 23:58:28 +0100

    Test: added test for the new support for custom express endpoints

--- test/scripts.index.spec.js
+++ test/scripts.index.spec.js
@@ -7,6 +7,7 @@ test('should return API methods', t => {
 	t.is(typeof api.build, 'function');
 	t.is(typeof api.server, 'function');
 	t.is(typeof api.makeWebpackConfig, 'function');
+	t.is(typeof api.createServer, 'function');
 });
 
 test('makeWebpackConfig should return development Webpack config', t => {
@@ -23,4 +24,29 @@ test('makeWebpackConfig should return production Webpack config', t => {
 	t.truthy(result);
 	t.is(result.output.filename, 'build/bundle.js');
 	t.false(result.cache);
+});
+
+test('createServer should return an object containing a server instance', t => {
+	const api = styleguidist({ components: '*.js' });
+	const result = api.createServer('production');
+	t.truthy(result);
+	t.truthy(result.app);
+});
+
+test('createServer should return an object containing a production Webpack compiler', t => {
+	const api = styleguidist({ components: '*.js' });
+	const result = api.createServer('production');
+	t.truthy(result);
+	t.truthy(result.compiler);
+	t.is(result.compiler.options.output.filename, 'build/bundle.js');
+	t.false(result.compiler.options.cache);
+});
+
+test('createServer should return an object containing a development Webpack compiler', t => {
+	const api = styleguidist({ components: '*.js' });
+	const result = api.createServer('development');
+	t.truthy(result);
+	t.truthy(result.compiler);
+	t.is(result.compiler.options.output.filename, 'build/bundle.js');
+	t.true(result.compiler.options.cache);
 });
