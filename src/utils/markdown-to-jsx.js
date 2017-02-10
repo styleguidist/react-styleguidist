0a534434ee9992c147695f948d5e149f38b261df
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2017-02-10 16:33:04 +0100

    Fix: Do not alter headings depth in user's *.md files
    Mutating user's documents can be very confusing. And since section's names are optional now,
    it's even less logical to start from `<h3>`.
    
    HTML5 standard also says when `<section>` are used, heading for each section can start with `h1`
    See: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML_sections_and_outlines

--- src/utils/markdown-to-jsx.js
+++ src/utils/markdown-to-jsx.js
@@ -1,7 +1,6 @@
 /*
  Based on https://github.com/yaycmyk/markdown-to-jsx
 
- - Increase level of heading by 2.
  - Server-rendered code highlight.
  - Custom className for code highlight.
  */
@@ -245,10 +244,8 @@ function getHTMLNodeTypeFromASTNodeType(node) {
 		case 'footnoteReference':
 			return 'a';
 
-		// Increase level of headings
 		case 'heading':
-			const depth = Math.min(node.depth + 2, 6);
-			return `h${depth}`;
+			return `h${node.depth}`;
 
 		case 'image':
 		case 'imageReference':
