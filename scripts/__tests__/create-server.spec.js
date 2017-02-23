8e6bdbcf9bf9ae3685fe0ed1b78b965403f41d4d
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2017-02-23 12:08:02 +0100

    Fixed tests for create-server

--- scripts/__tests__/create-server.spec.js
+++ scripts/__tests__/create-server.spec.js
@@ -14,22 +14,29 @@ test('createServer should return an object containing a server instance', () => 
 	expect(result.app).toBeTruthy();
 });
 
-test('createServer should return an object containing a production Webpack compiler', () => {
+test('createServer should return an object containing a production Webpack multi-compiler', () => {
 	process.chdir('test/apps/basic');
 	const config = getConfig();
 	const result = createServer(config, 'production');
 	expect(result).toBeTruthy();
+	expect(result).toBeTruthy();
 	expect(result.compiler).toBeTruthy();
-	expect(result.compiler.options.output.filename).toBe('build/[name].js');
-	expect(result.compiler.options.cache).toBeFalsy();
+	expect(result.compiler.compilers).toHaveLength(2);
+	expect(result.compiler.compilers[0].options.output.filename).toBe('build/[name].js');
+	expect(result.compiler.compilers[0].options.cache).toBeFalsy();
+	expect(result.compiler.compilers[1].options.output.filename).toBe('build/[name].js');
+	expect(result.compiler.compilers[1].options.cache).toBeFalsy();
 });
 
-test('createServer should return an object containing a development Webpack compiler', () => {
+test('createServer should return an object containing a development Webpack multi-compiler', () => {
 	process.chdir('test/apps/basic');
 	const config = getConfig();
 	const result = createServer(config, 'development');
 	expect(result).toBeTruthy();
 	expect(result.compiler).toBeTruthy();
-	expect(result.compiler.options.output.filename).toBe('build/[name].js');
-	expect(result.compiler.options.cache).toBeTruthy();
+	expect(result.compiler.compilers).toHaveLength(2);
+	expect(result.compiler.compilers[0].options.output.filename).toBe('build/[name].js');
+	expect(result.compiler.compilers[0].options.cache).toBeTruthy();
+	expect(result.compiler.compilers[1].options.output.filename).toBe('build/[name].js');
+	expect(result.compiler.compilers[1].options.cache).toBeTruthy();
 });
