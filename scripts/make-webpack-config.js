2a6cc31dec74aab1b2cd61163531cc191e9f6bc4
Andrew Walton <andrew@greenarrow.me>
2017-04-05 02:35:31 -0400

    Fix: Hash JS assets in production build (#372)
    * Hash JS assets in production build
    
    * Clean build directory during webpack build

--- scripts/make-webpack-config.js
+++ scripts/make-webpack-config.js
@@ -6,6 +6,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const hasJsonLoader = require('./utils/hasJsonLoader');
 const getWebpackVersion = require('./utils/getWebpackVersion');
 const mergeWebpackConfig = require('./utils/mergeWebpackConfig');
@@ -55,6 +56,10 @@ module.exports = function(config, env) {
 
 	if (isProd) {
 		webpackConfig = merge(webpackConfig, {
+			output: {
+				filename: 'build/bundle.[chunkhash:8].js',
+				chunkFilename: 'build/[name].[chunkhash:8].js',
+			},
 			plugins: [
 				new webpack.optimize.OccurrenceOrderPlugin(),
 				new webpack.optimize.UglifyJsPlugin({
@@ -69,6 +74,9 @@ module.exports = function(config, env) {
 					mangle: {
 						keep_fnames: true,
 					},
+				}),
+				new CleanWebpackPlugin(['build'], {
+					root: config.styleguideDir,
 				}),
 			],
 		});
