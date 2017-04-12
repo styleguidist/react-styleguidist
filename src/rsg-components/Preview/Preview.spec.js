227ec54683eb0a5dad580e1cc210186844b3a026
Dylan Mozlowski <dmozlowski@classy.org>
2017-04-12 15:20:13 -0700

    Add context to Preview spec

--- src/rsg-components/Preview/Preview.spec.js
+++ src/rsg-components/Preview/Preview.spec.js
@@ -3,13 +3,21 @@ import noop from 'lodash/noop';
 import Preview from '../Preview';
 
 const code = '<button>OK</button>';
+const options = {
+	context: {
+		config: {
+			bubleConfig: {},
+		},
+	},
+};
 
 it('should render component renderer', () => {
 	const actual = shallow(
 		<Preview
 			code={code}
 			evalInContext={noop}
-		/>
+		/>,
+		options
 	);
 
 	expect(actual).toMatchSnapshot();
