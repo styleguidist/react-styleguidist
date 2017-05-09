0ec6e96ca72058ec98d2830f9ed65f0e3b30d117
Sara Vieira <sara.vieira@mindera.com>
2017-05-09 13:47:31 +0100

    fix(235) - Move open isolated

--- src/rsg-components/Playground/PlaygroundRenderer.js
+++ src/rsg-components/Playground/PlaygroundRenderer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { MdFullscreen, MdFullscreenExit, MdArrowDropDown, MdArrowDropUp } from 'react-icons/lib/md';
 import Editor from 'rsg-components/Editor';
 import Link from 'rsg-components/Link';
 import Preview from 'rsg-components/Preview';
@@ -13,51 +14,42 @@ const styles = ({ space, color, fontFamily, fontSize, borderRadius }) => ({
 		border: [[1, color.border, 'solid']],
 		borderRadius: [[borderRadius, borderRadius, 0, borderRadius]],
 		marginTop: space[0],
-		'&:hover $isolatedLink': {
-			isolate: false,
-			opacity: 1,
-		},
 	},
 	preview: {
 		marginBottom: space[0],
 		padding: space[2],
 	},
 	codeToggle: {
-		position: 'absolute',
-		right: -1,
-		margin: 0,
-		padding: [[space[0], space[1]]],
+		outline: 'none',
+		border: 'none',
 		fontFamily: fontFamily.base,
 		fontSize: fontSize.base,
-		lineHeight: 1,
-		color: color.link,
-		border: [[1, color.border, 'solid']],
-		borderTop: 0,
-		borderBottomLeftRadius: borderRadius,
-		borderBottomRightRadius: borderRadius,
+		color: color.light,
+		transition: 'all 200ms ease',
 		cursor: 'pointer',
-		'&:hover, &:active': {
-			isolate: false,
+		'&:hover': {
 			color: color.linkHover,
+			fontFamily: fontFamily.base,
+			fontSize: fontSize.base,
 		},
 	},
-	showCode: {
-		composes: '$codeToggle',
-		backgroundColor: color.baseBackground,
+	toolbar: {
+		display: 'flex',
+		padding: [[space[0], space[2]]],
+		'& > *:not(:last-child)': {
+			marginRight: space[1],
+		},
 	},
-	hideCode: {
-		composes: '$codeToggle',
-		backgroundColor: color.codeBackground,
-	},
-	isolatedLink: {
-		position: 'absolute',
-		top: 0,
-		right: 0,
-		padding: [[space[0], space[1]]],
-		fontFamily: fontFamily.base,
-		fontSize: fontSize.base,
-		opacity: 0,
-		transition: 'opacity ease-in-out .15s .2s',
+	icons: {
+		width: 20,
+		height: 20,
+		color: color.light,
+		transition: 'all 200ms ease',
+		'&:hover': {
+			width: 20,
+			height: 20,
+			color: color.linkHover,
+		},
 	},
 });
 
@@ -75,29 +67,29 @@ export function PlaygroundRenderer({
 	return (
 		<div className={classes.root}>
 			<div className={classes.preview} data-preview={name ? name : ''}>
-				<div className={classes.isolatedLink}>
-					{name && (
-						isolatedExample ? (
-							<Link href={'#!/' + name}>⇽ Exit Isolation</Link>
-						) : (
-							<Link href={'#!/' + name + '/' + index}>Open isolated ⇢</Link>
-						)
-					)}
-				</div>
 				<Preview code={code} evalInContext={evalInContext} />
 			</div>
-			{showCode ? (
+			<section className={classes.toolbar}>
 				<div>
-					<Editor code={code} onChange={onChange} />
-					<button type="button" className={classes.hideCode} onClick={onCodeToggle}>
-						Hide code
-					</button>
+					{name && (
+					isolatedExample ? (
+						<Link href={'/'}><MdFullscreenExit className={classes.icons} /></Link>
+					) : (
+						<Link href={'#!/' + name + '/' + index}>
+							<MdFullscreen className={classes.icons} />
+						</Link>
+					)
+				)}
 				</div>
-			) : (
-				<button type="button" className={classes.showCode} onClick={onCodeToggle}>
-					Show code
+				<button type="button" className={classes.codeToggle} onClick={onCodeToggle}>
+					{ showCode ? 'Hide' : 'Show'} code
+							{ showCode
+								? <MdArrowDropUp className={classes.icons} />
+								: <MdArrowDropDown className={classes.icons} />
+							}
 				</button>
-			)}
+			</section>
+			{showCode && <Editor code={code} onChange={onChange} />}
 		</div>
 	);
 }
