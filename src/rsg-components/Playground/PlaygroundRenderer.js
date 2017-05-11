9164b592a21ce1a45a0735bdc53d4cb09a704871
Sara Vieira <hey@iamsaravieira.com>
2017-05-11 13:00:52 +0100

    fix(235) - Move open isolated (#434)
    * fix(235) - Move open isolated
    
    * add isolate
    
    * separate imports
    
    * Add react group , update other isolated link

--- src/rsg-components/Playground/PlaygroundRenderer.js
+++ src/rsg-components/Playground/PlaygroundRenderer.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Group from 'react-group';
 import PropTypes from 'prop-types';
+import MdFullscreen from 'react-icons/lib/md/fullscreen';
+import MdFullscreenExit from 'react-icons/lib/md/fullscreen-exit';
+import MdArrowDropDown from 'react-icons/lib/md/arrow-drop-down';
+import MdArrowDropUp from 'react-icons/lib/md/arrow-drop-up';
 import Editor from 'rsg-components/Editor';
 import Link from 'rsg-components/Link';
 import Preview from 'rsg-components/Preview';
@@ -13,51 +18,43 @@ const styles = ({ space, color, fontFamily, fontSize, borderRadius }) => ({
 		border: [[1, color.border, 'solid']],
 		borderRadius: [[borderRadius, borderRadius, 0, borderRadius]],
 		marginTop: space[0],
-		'&:hover $isolatedLink': {
-			isolate: false,
-			opacity: 1,
-		},
 	},
 	preview: {
 		marginBottom: space[0],
 		padding: space[2],
 	},
 	codeToggle: {
-		position: 'absolute',
-		right: -1,
-		margin: 0,
-		padding: [[space[0], space[1]]],
+		border: 'none',
 		fontFamily: fontFamily.base,
 		fontSize: fontSize.base,
-		lineHeight: 1,
-		color: color.link,
-		border: [[1, color.border, 'solid']],
-		borderTop: 0,
-		borderBottomLeftRadius: borderRadius,
-		borderBottomRightRadius: borderRadius,
+		color: color.light,
+		transition: 'all 200ms ease',
 		cursor: 'pointer',
-		'&:hover, &:active': {
+		'&:not(:focus)': {
+			isolate: false,
+			outline: 'none',
+		},
+		'&:hover': {
 			isolate: false,
 			color: color.linkHover,
 		},
 	},
-	showCode: {
-		composes: '$codeToggle',
-		backgroundColor: color.baseBackground,
+	toolbar: {
+		display: 'flex',
+		padding: [[space[0], space[2]]],
 	},
-	hideCode: {
-		composes: '$codeToggle',
-		backgroundColor: color.codeBackground,
+	toolbarItem: {
+		marginRight: space[1],
 	},
-	isolatedLink: {
-		position: 'absolute',
-		top: 0,
-		right: 0,
-		padding: [[space[0], space[1]]],
-		fontFamily: fontFamily.base,
-		fontSize: fontSize.base,
-		opacity: 0,
-		transition: 'opacity ease-in-out .15s .2s',
+	icons: {
+		width: 20,
+		height: 20,
+		color: color.light,
+		transition: 'all 200ms ease',
+		'&:hover': {
+			isolate: false,
+			color: color.linkHover,
+		},
 	},
 });
 
@@ -75,29 +72,31 @@ export function PlaygroundRenderer({
 	return (
 		<div className={classes.root}>
 			<div className={classes.preview} data-preview={name ? name : ''}>
-				<div className={classes.isolatedLink}>
-					{name && (
-						isolatedExample ? (
-							<Link href={'#!/' + name}>⇽ Exit Isolation</Link>
-						) : (
-							<Link href={'#!/' + name + '/' + index}>Open isolated ⇢</Link>
-						)
-					)}
-				</div>
 				<Preview code={code} evalInContext={evalInContext} />
 			</div>
-			{showCode ? (
-				<div>
-					<Editor code={code} onChange={onChange} />
-					<button type="button" className={classes.hideCode} onClick={onCodeToggle}>
-						Hide code
+			<Group separator=" " className={classes.toolbar}>
+				<div className={classes.toolbarItem}>
+					{name && (
+					isolatedExample ? (
+						<Link href={'/'}><MdFullscreenExit className={classes.icons} /></Link>
+					) : (
+						<Link href={'#!/' + name + '/' + index}>
+							<MdFullscreen className={classes.icons} />
+						</Link>
+					)
+				)}
+				</div>
+				<div className={classes.toolbarItem}>
+					<button type="button" className={classes.codeToggle} onClick={onCodeToggle}>
+						{ showCode ? 'Hide' : 'Show'} code
+							{ showCode
+								? <MdArrowDropUp className={classes.icons} />
+								: <MdArrowDropDown className={classes.icons} />
+							}
 					</button>
 				</div>
-			) : (
-				<button type="button" className={classes.showCode} onClick={onCodeToggle}>
-					Show code
-				</button>
-			)}
+			</Group>
+			{showCode && <Editor code={code} onChange={onChange} />}
 		</div>
 	);
 }
--- src/rsg-components/ReactComponent/ReactComponentRenderer.js
+++ src/rsg-components/ReactComponent/ReactComponentRenderer.js
@@ -6,6 +6,8 @@ import Markdown from 'rsg-components/Markdown';
 import JsDoc from 'rsg-components/JsDoc';
 import Styled from 'rsg-components/Styled';
 import cx from 'classnames';
+import MdFullscreen from 'react-icons/lib/md/fullscreen';
+import MdFullscreenExit from 'react-icons/lib/md/fullscreen-exit';
 
 const styles = ({ color, fontSize, fontFamily, space }) => ({
 	root: {
@@ -20,14 +22,15 @@ const styles = ({ color, fontSize, fontFamily, space }) => ({
 		position: 'relative',
 		marginBottom: space[3],
 	},
-	isolatedLink: {
-		position: 'absolute',
-		top: 0,
-		right: 0,
-		fontFamily: fontFamily.base,
-		fontSize: fontSize.base,
-		opacity: 0,
-		transition: 'opacity ease-in-out .15s .2s',
+	icons: {
+		width: 30,
+		height: 30,
+		color: color.light,
+		transition: 'all 200ms ease',
+		'&:hover': {
+			isolate: false,
+			color: color.linkHover,
+		},
 	},
 	primaryHeading: {
 		color: color.base,
@@ -37,6 +40,9 @@ const styles = ({ color, fontSize, fontFamily, space }) => ({
 		fontFamily: fontFamily.base,
 		fontSize: fontSize.h2,
 		fontWeight: 'normal',
+		display: 'flex',
+		alignItem: 'center',
+		justifyContent: 'space-between',
 	},
 	heading: {
 		color: color.base,
@@ -84,15 +90,19 @@ export function ReactComponentRenderer({
 			<header className={classes.header}>
 				<Heading level={2} className={headingClasses} slug={slug}>
 					{name}
+					<div className={classes.isolatedLink}>
+						{isolated ? (
+							<Link href="/">
+								<MdFullscreenExit className={classes.icons} />
+							</Link>
+						) : (
+							<Link href={'#!/' + name}>
+								<MdFullscreen className={classes.icons} />
+							</Link>
+						)}
+					</div>
 				</Heading>
 				<div className={classes.pathLine}>{pathLine}</div>
-				<div className={classes.isolatedLink}>
-					{isolated ? (
-						<Link href="/">← Back</Link>
-					) : (
-						<Link href={'#!/' + name}>Open isolated ⇢</Link>
-					)}
-				</div>
 			</header>
 			<div className={classes.description}>
 				{description && <Markdown text={description} />}
