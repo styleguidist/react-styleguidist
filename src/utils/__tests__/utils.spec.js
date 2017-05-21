9fa27621b7d568ddac9dc913da3c09f1661c2205
Artem Sapegin <artem@sapegin.ru>
2017-05-21 10:40:56 +0200

    Refactor: Add getUrl util function

--- src/utils/__tests__/utils.spec.js
+++ src/utils/__tests__/utils.spec.js
@@ -299,3 +299,62 @@ describe('filterComponentExamples', () => {
 		});
 	});
 });
+
+describe('getUrl', () => {
+	const loc = {
+		origin: 'http://example.com',
+		pathname: '/styleguide/',
+	};
+	const name = 'FooBar';
+	const slug = 'foobar';
+
+	it('should return a home URL', () => {
+		const result = utils.getUrl({}, loc);
+		expect(result).toBe('/styleguide/');
+	});
+
+	it('should return an absolute home URL', () => {
+		const result = utils.getUrl({ absolute: true }, loc);
+		expect(result).toBe('http://example.com/styleguide/');
+	});
+
+	it('should return an anchor URL', () => {
+		const result = utils.getUrl({ name, slug, anchor: true }, loc);
+		expect(result).toBe('/styleguide/#foobar');
+	});
+
+	it('should return an absolute anchor URL', () => {
+		const result = utils.getUrl({ name, slug, anchor: true, absolute: true }, loc);
+		expect(result).toBe('http://example.com/styleguide/#foobar');
+	});
+
+	it('should return an isolated URL', () => {
+		const result = utils.getUrl({ name, slug, isolated: true }, loc);
+		expect(result).toBe('/styleguide/#!/FooBar');
+	});
+
+	it('should return an absolute isolated URL', () => {
+		const result = utils.getUrl({ name, slug, isolated: true, absolute: true }, loc);
+		expect(result).toBe('http://example.com/styleguide/#!/FooBar');
+	});
+
+	it('should return an isolated example URL', () => {
+		const result = utils.getUrl({ name, slug, example: '3', isolated: true }, loc);
+		expect(result).toBe('/styleguide/#!/FooBar/3');
+	});
+
+	it('should return an absolute isolated example URL', () => {
+		const result = utils.getUrl({ name, slug, example: '3', isolated: true, absolute: true }, loc);
+		expect(result).toBe('http://example.com/styleguide/#!/FooBar/3');
+	});
+
+	it('should return a nochrome URL', () => {
+		const result = utils.getUrl({ name, slug, nochrome: true }, loc);
+		expect(result).toBe('/styleguide/?nochrome#!/FooBar');
+	});
+
+	it('should return an absolute nochrome URL', () => {
+		const result = utils.getUrl({ name, slug, nochrome: true, absolute: true }, loc);
+		expect(result).toBe('http://example.com/styleguide/?nochrome#!/FooBar');
+	});
+});
