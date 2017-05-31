styleguidist/react-styleguidist

commit 4e815924edee34f7e9661219283f8fa076ca32c5

Fix: Add .jsx extension to default components and ignore patterns (#404)

scripts/schemas/config.js
@@ -5,7 +5,7 @@
 
 /* eslint-disable no-console */
 
-const DEFAULT_COMPONENTS_PATTERN = 'src/@(components|Components)/**/*.js';
+const DEFAULT_COMPONENTS_PATTERN = 'src/@(components|Components)/**/*.jsx?';
 
 const fs = require('fs');
 const path = require('path');
@@ -30,7 +30,7 @@ module.exports = {
 	// `components` is a shortcut for { sections: [{ components }] }, see `sections` below
 	components: {
 		type: ['string', 'function'],
-		example: 'components/**/[A-Z]*.js',
+		example: 'components/**/[A-Z]*.jsx?',
 	},
 	context: {
 		type: 'object',
@@ -85,6 +85,8 @@ module.exports = {
 			'**/__tests__/**',
 			'**/*.test.js',
 			'**/*.spec.js',
+			'**/*.test.jsx',
+			'**/*.spec.jsx'
 		],
 	},
 	highlightTheme: {

commit 99ee52c12ad2af7e3810e2b7f26754f33e7d245c

Fix: Fix incorrect default glob pattern (was introduced in #404, fixes #461)

scripts/schemas/config.js
@@ -5,7 +5,7 @@
 
 /* eslint-disable no-console */
 
-const DEFAULT_COMPONENTS_PATTERN = 'src/@(components|Components)/**/*.jsx?';
+const DEFAULT_COMPONENTS_PATTERN = 'src/@(components|Components)/**/*.{js,jsx}';
 
 const fs = require('fs');
 const path = require('path');
@@ -31,7 +31,7 @@ module.exports = {
 	// `components` is a shortcut for { sections: [{ components }] }, see `sections` below
 	components: {
 		type: ['string', 'function'],
-		example: 'components/**/[A-Z]*.jsx?',
+		example: 'components/**/[A-Z]*.js',
 	},
 	context: {
 		type: 'object',

loaders/__tests__/styleguide-loader.spec.js
@@ -1,8 +1,8 @@
 import vm from 'vm';
 import path from 'path';
 import { readFileSync } from 'fs';
-import noop from 'lodash/noop';
 import styleguideLoader from '../styleguide-loader';
+import getConfig from '../../scripts/config';
 
 const file = './test/components/Button/Button.js';
 
@@ -18,12 +18,31 @@ it('should return valid, parsable JS', () => {
 				getExampleFilename: () => 'Readme.md',
 				getComponentPathLine: filepath => filepath,
 			},
-			addContextDependency: noop,
+			addContextDependency: () => {},
 		},
 		readFileSync(file, 'utf8')
 	);
 	expect(result).toBeTruthy();
 	expect(() => new vm.Script(result)).not.toThrow();
+});
+
+it('should return correct component paths: default glob pattern', () => {
+	const result = styleguideLoader.pitch.call(
+		{
+			request: file,
+			_styleguidist: {
+				...getConfig(),
+				configDir: path.resolve(__dirname, '../../examples/cra'),
+			},
+			addContextDependency: () => {},
+		},
+		readFileSync(file, 'utf8')
+	);
+	expect(result).toBeTruthy();
+	expect(() => new vm.Script(result)).not.toThrow();
+	expect(result).toMatch(`'filepath': 'src/components/Button.js'`);
+	expect(result).toMatch(`'filepath': 'src/components/Placeholder.js'`);
+	expect(result).toMatch(`'filepath': 'src/components/RandomButton.js'`);
 });
 
 it('should return correct component paths: glob', () => {
@@ -36,7 +55,7 @@ it('should return correct component paths: glob', () => {
 				getExampleFilename: () => 'Readme.md',
 				getComponentPathLine: filepath => filepath,
 			},
-			addContextDependency: noop,
+			addContextDependency: () => {},
 		},
 		readFileSync(file, 'utf8')
 	);
@@ -63,7 +82,7 @@ it('should return correct component paths: function returning absolute paths', (
 				getExampleFilename: () => 'Readme.md',
 				getComponentPathLine: filepath => filepath,
 			},
-			addContextDependency: noop,
+			addContextDependency: () => {},
 		},
 		readFileSync(file, 'utf8')
 	);
@@ -90,7 +109,7 @@ it('should return correct component paths: function returning relative paths', (
 				getExampleFilename: () => 'Readme.md',
 				getComponentPathLine: filepath => filepath,
 			},
-			addContextDependency: noop,
+			addContextDependency: () => {},
 		},
 		readFileSync(file, 'utf8')
 	);
@@ -118,7 +137,7 @@ it('should filter out components without examples if skipComponentsWithoutExampl
 				getExampleFilename: componentPath => path.join(path.dirname(componentPath), 'Readme.md'),
 				getComponentPathLine: filepath => filepath,
 			},
-			addContextDependency: noop,
+			addContextDependency: () => {},
 		},
 		readFileSync(file, 'utf8')
 	);

scripts/__tests__/config.spec.js
@@ -134,7 +134,7 @@ it('should use components option as the first sections if there’s no sections 
 it('should use default components option both components and sections options weren’t specified', () => {
 	const result = getConfig();
 	expect(result.sections).toHaveLength(1);
-	expect(result.sections[0].components).toMatch('.js');
+	expect(result.sections[0].components).toMatch('**');
 });
 
 it('should ignore components option there’s sections options', () => {