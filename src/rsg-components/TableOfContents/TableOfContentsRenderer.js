262d5f0118e935276d5c61fbde06083bdfc1ed7e
Artem Sapegin <artem@sapegin.ru>
2017-06-10 21:55:32 +0200

    Fix: Isolate input placeholder styles (#481)

--- src/rsg-components/TableOfContents/TableOfContentsRenderer.js
+++ src/rsg-components/TableOfContents/TableOfContentsRenderer.js
@@ -25,6 +25,11 @@ const styles = ({ space, color, fontFamily, fontSize, borderRadius }) => ({
 			borderColor: color.link,
 			outline: 0,
 		},
+		'&::placeholder': {
+			fontFamily: fontFamily.base,
+			fontSize: fontSize.base,
+			color: color.light,
+		},
 	},
 });
 
