7632f8959b9f5ae91685d27101e3f8bcbf8c11f7
Sara Vieira <sara.vieira@mindera.com>
2017-06-11 17:14:49 +0100

    fix some errors

--- test/phantom.js
+++ test/phantom.js
@@ -1,12 +1,10 @@
-const system = require('system');
-
 // To make a screenshot: https://github.com/ariya/phantomjs/blob/master/examples/rasterize.js
 // phantomjs rasterize.js http://localhost:6060/ output.png
 
 /* eslint-disable no-console, no-multi-assign */
 /* globals phantom:false */
 
-if (system.args.length !== 2) {
+if (process.argv.args.length !== 2) {
 	console.log('Usage: phantom.js <URL>');
 	phantom.exit(1);
 }
@@ -40,7 +38,7 @@ page.onConsoleMessage = function(msg) {
 
 page.viewportSize = { width: 1024, height: 768 };
 
-page.open(system.args[1], function(status) {
+page.open(process.argv.args[1], function(status) {
 	if (status !== 'success') {
 		console.log('PhantomJS: Cannot load the page');
 		phantom.exit(1);
