0d0ca5aec0ec6c4de8ee54f696d38cd4f5b5df5c
Sara Vieira <hey@iamsaravieira.com>
2017-06-12 13:09:53 +0100

    Fix: Only show Props & methods tab button if there are props (#488, close #486)

--- src/rsg-components/slots/UsageTabButton.js
+++ src/rsg-components/slots/UsageTabButton.js
@@ -2,14 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TabButton from 'rsg-components/TabButton';
 
-const UsageTabButton = props =>
-	<TabButton {...props}>
-		Props & methods
-	</TabButton>;
+const UsageTabButton = props => {
+	const component = props.props;
+	const showButton = component.props || (component.methods && component.methods.length > 0);
+	return showButton
+		? <TabButton {...props}>
+				Props & methods
+			</TabButton>
+		: null;
+};
 
 UsageTabButton.propTypes = {
 	onClick: PropTypes.func.isRequired,
 	name: PropTypes.string.isRequired,
+	props: PropTypes.shape({
+		props: PropTypes.object,
+		methods: PropTypes.array,
+	}).isRequired,
 	active: PropTypes.bool,
 };
 
