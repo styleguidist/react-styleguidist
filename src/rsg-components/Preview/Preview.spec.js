b214d69a22a4c536d1b3659e33cd5dc7ad158bdb
Wenbo Yu <wyu@box.com>
2017-07-10 17:40:39 -0700

    more tests

--- src/rsg-components/Preview/Preview.spec.js
+++ src/rsg-components/Preview/Preview.spec.js
@@ -11,8 +11,9 @@ const options = {
 	},
 };
 
-it('should unmount Wrapper when Preview unmounts', () => {
+it('should unmount Wrapper when Preview unmounts and mountNodeMock is not null', () => {
 	const actual = mount(<Preview code={code} evalInContext={() => noop} />, options);
+	actual.instance().mountNode = { hi: 1 };
 	const ReactDOM = require('react-dom');
 	const unmountComponentAtNodeMock = jest.fn();
 	ReactDOM.unmountComponentAtNode = unmountComponentAtNodeMock;
@@ -20,6 +21,18 @@ it('should unmount Wrapper when Preview unmounts', () => {
 	actual.unmount();
 
 	expect(unmountComponentAtNodeMock).toBeCalled();
+});
+
+it('should not unmount Wrapper when Preview unmounts and mountNodeMock is null', () => {
+	const actual = mount(<Preview code={code} evalInContext={() => noop} />, options);
+	actual.instance().mountNode = null;
+	const ReactDOM = require('react-dom');
+	const unmountComponentAtNodeMock = jest.fn();
+	ReactDOM.unmountComponentAtNode = unmountComponentAtNodeMock;
+
+	actual.unmount();
+
+	expect(unmountComponentAtNodeMock).not.toBeCalled();
 });
 
 it('should render component renderer', () => {
