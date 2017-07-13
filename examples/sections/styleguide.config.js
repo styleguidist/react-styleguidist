96d27f389a7c88bca0dcef8551176ad387e5e4af
Artem Sapegin <artem@sapegin.ru>
2017-07-13 11:22:45 +0200

    Chore: Increase webpack bundle size limit
    webpack 3 produces bigger bundle than webpack 2

--- examples/sections/styleguide.config.js
+++ examples/sections/styleguide.config.js
@@ -31,8 +31,8 @@ module.exports = {
 			env === 'development'
 				? false
 				: {
-						maxAssetSize: 670000, // bytes
-						maxEntrypointSize: 670000, // bytes
+						maxAssetSize: 685000, // bytes
+						maxEntrypointSize: 685000, // bytes
 						hints: 'error',
 					},
 	}),
