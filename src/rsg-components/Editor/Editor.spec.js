2dd2827a31b022a2aad12bc35bb8d15ead4b7463
Artem Sapegin <artem@sapegin.ru>
2017-08-16 10:53:47 +0200

    Test: Add some tests for Editor component

--- src/rsg-components/Editor/Editor.spec.js
+++ src/rsg-components/Editor/Editor.spec.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { EditorLoaderRenderer } from './EditorLoaderRenderer';
+import Editor from './Editor';
 
-// We do not test Editor component because it requires Codemirror that do not work in Node environment.
+const code = '<button>MyAwesomeCode</button>';
+const newCode = '<button>MyNewAwesomeCode</button>';
+const options = {
+	context: {
+		config: {
+			showCode: false,
+			highlightTheme: 'base16-light',
+		},
+	},
+	childContextTypes: {
+		config: PropTypes.object.isRequired,
+	},
+};
 
-it('renderer should render loader', () => {
-	const actual = shallow(<EditorLoaderRenderer classes={{}} />);
+describe('EditorLoaderRenderer', () => {
+	it('should renderer should render loader', () => {
+		const actual = shallow(<EditorLoaderRenderer classes={{}} />);
 
-	expect(actual).toMatchSnapshot();
+		expect(actual).toMatchSnapshot();
+	});
+});
+
+describe('Editor', () => {
+	it('should renderer and editor', () => {
+		const actual = shallow(<Editor code={code} onChange={() => {}} />, options);
+
+		expect(actual).toMatchSnapshot();
+	});
+
+	it('should update code with debounce', done => {
+		const onChange = jest.fn();
+		const actual = mount(<Editor code={code} onChange={onChange} />, options);
+
+		expect(actual.text()).toMatch(code);
+
+		// Set new value by calling a method on the CodeMirror instance
+		actual.find('div').node.querySelector('.CodeMirror').CodeMirror.setValue(newCode);
+
+		setTimeout(() => {
+			expect(onChange).toBeCalledWith(newCode);
+			done();
+		}, 13);
+	});
 });
