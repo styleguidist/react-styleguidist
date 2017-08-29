cd2ad5b3f79ae2f7a7f3472e412388193f2f3920
Viacheslav Avsenev <y_o_z@mail.ru>
2017-08-29 15:54:09 +0300

    #470 reuse react dev-tools

--- scripts/utils/__tests__/devServerUtils.spec.js
+++ scripts/utils/__tests__/devServerUtils.spec.js
@@ -1,13 +1,5 @@
 import devServerUtils from '../devServerUtils';
 
-it('should return a correct address', () => {
-	const result = devServerUtils.prepareUrls('http', '0.0.0.0', '6060');
-	expect(result.localUrlForTerminal).toBeTruthy();
-	expect(result.localUrlForBrowser).toBeTruthy();
-	expect(result.lanUrlForConfig).toBeTruthy();
-	expect(result.lanUrlForTerminal).toBeTruthy();
-});
-
 it('Main method doesn’t throw error', () => {
 	const fn = () => devServerUtils.printInstructions();
 	expect(fn).not.toThrowError();
