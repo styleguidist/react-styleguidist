a7a5063b0a157858a1ace8ea772ee6650cb139fb
Viacheslav Avsenev <y_o_z@mail.ru>
2017-08-29 16:55:29 +0300

    #470 rename printInstructions() parameter

--- scripts/utils/devServerUtils.js
+++ scripts/utils/devServerUtils.js
@@ -4,8 +4,8 @@ const chalk = require('chalk');
 const logger = require('glogg')('rsg');
 const webpackDevServerUtils = require('react-dev-utils/WebpackDevServerUtils');
 
-function printInstructions(protocol, host, port) {
-	const urls = webpackDevServerUtils.prepareUrls(protocol ? 'https' : 'http', host, port);
+function printInstructions(isHttps, host, port) {
+	const urls = webpackDevServerUtils.prepareUrls(isHttps ? 'https' : 'http', host, port);
 	logger.info(`You can now view style guide in the browser.`);
 
 	logger.info(`  ${chalk.bold('Local:')}            ${urls.localUrlForTerminal}`);
