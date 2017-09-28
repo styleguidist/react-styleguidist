2534af7c9915b9380def4fc33cc535a59fe5a205
Gus Caplan <me@gus.host>
2017-09-28 12:30:49 -0500

    add page error handler

--- test/browser.js
+++ test/browser.js
@@ -5,15 +5,19 @@ const path = require('path');
 
 const args = process.argv.slice(2);
 
+async function onerror(browser, err) {
+	console.error(err.stack);
+	await browser.close();
+	process.exit(1);
+}
+
 (async () => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await page.setViewport({ width: 1024, height: 768 });
-	page.on('error', async err => {
-		console.error(err.stack);
-		await browser.close();
-		process.exit(1);
-	});
+	page.on('error', e => onerror(browser, e));
+	page.on('pageerror', e => onerror(browser, e));
+
 	page.on('console', (...args) => console.log('PAGE LOG:', ...args));
 
 	const url = /https?/.test(args[0]) ? args[0] : `file://${path.resolve(args[0])}`;
