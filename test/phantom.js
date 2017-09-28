07ed17a3b899dff24d686736652a662e1ba2b214
Gus Caplan <me@gus.host>
2017-09-28 12:26:41 -0500

    move phantom to puppeteer

--- test/phantom.js
+++ test/phantom.js
@@ -1,49 +1,29 @@
-// To make a screenshot: https://github.com/ariya/phantomjs/blob/master/examples/rasterize.js
-// phantomjs rasterize.js http://localhost:6060/ output.png
+/* eslint-disable no-console */
 
-/* eslint-disable no-console, no-multi-assign */
-/* globals phantom:false */
+const puppeteer = require('puppeteer');
+const path = require('path');
 
-const system = require('system');
+const args = process.argv.slice(2);
 
-if (system.args.length !== 2) {
-	console.log('Usage: phantom.js <URL>');
-	phantom.exit(1);
-}
+(async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.setViewport({ width: 1024, height: 768 });
+	page.on('error', async err => {
+		console.error(err.stack);
+		await browser.close();
+		process.exit(1);
+	});
+	page.on('console', (...args) => console.log('PAGE LOG:', ...args));
 
-const page = require('webpage').create();
+	const url = /https?/.test(args[0]) ? args[0] : `file://${path.resolve(args[0])}`;
+	await page.goto(url);
 
-page.onError = phantom.onError = function(err, trace) {
-	console.log('PhantomJS error:', err);
-	if (trace && trace.length > 0) {
-		trace.forEach(function(t) {
-			console.log(
-				' -> ' +
-					(t.file || t.sourceURL) +
-					': ' +
-					t.line +
-					(t.function ? ' (in function ' + t.function + ')' : '')
-			);
-		});
+	if (args[1]) {
+		await page.screenshot({ path: args[1] });
 	}
-	phantom.exit(1);
-};
-page.onResourceError = page.onResourceTimeout = function(err) {
-	if (err.url.startsWith('http://placebeard.it/')) {
-		return;
-	}
-	console.log('PhantomJS cannot load resource:', err.url, '-', err.errorString);
-};
-page.onConsoleMessage = function(msg) {
-	console.log('PhantomJS:', msg);
-};
 
-page.viewportSize = { width: 1024, height: 768 };
-
-page.open(system.args[1], function(status) {
-	if (status !== 'success') {
-		console.log('PhantomJS: Cannot load the page');
-		phantom.exit(1);
-	}
-	phantom.exit(0);
+	await browser.close();
+})().catch(err => {
+	console.error('Browser error', err);
 });
