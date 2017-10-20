f726380d559cb00ca2dff9435207fcaa1f22c7e1
Ryan Oglesby <ryan.oglesby08@gmail.com>
2017-10-20 15:54:25 -0500

    Add a Heading component and use it in Markdown.
    Also need a MarkdownHeading component becuase extra margin gets
    introduced only in Markdown blocks.

---
+++ src/rsg-components/Markdown/MarkdownHeading/MarkdownHeadingRenderer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Styled from 'rsg-components/Styled';
+import Heading from 'rsg-components/Heading';
+
+const styles = ({ space }) => ({
+	markdownHeading: {
+		marginTop: 0,
+		marginBottom: space[2],
+	},
+});
+
+function MarkdownHeadingRenderer({ classes, level, children }) {
+	return (
+		<div className={classes.markdownHeading}>
+			<Heading level={level}>{children}</Heading>
+		</div>
+	);
+}
+
+MarkdownHeadingRenderer.propTypes = {
+	classes: PropTypes.object.isRequired,
+	level: PropTypes.oneOf([1, 2, 3, 4, 5, 6]).isRequired,
+	children: PropTypes.node,
+};
+
+export default Styled(styles)(MarkdownHeadingRenderer);
