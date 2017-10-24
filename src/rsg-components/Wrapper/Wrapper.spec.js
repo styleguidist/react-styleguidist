a92f770e647b13bce0821af96b3fc1de7ad6053b
Jeremy Gayed <tizmagik@yahoo.com>
2017-10-24 00:43:47 -0400

    Fix test

--- src/rsg-components/Wrapper/Wrapper.spec.js
+++ src/rsg-components/Wrapper/Wrapper.spec.js
@@ -3,7 +3,7 @@ import Wrapper from './Wrapper';
 
 it('should render children', () => {
 	const children = <span>Hello</span>;
-	const actual = shallow(<Wrapper>{children}</Wrapper>);
+	const actual = shallow(<Wrapper onError={() => {}}>{children}</Wrapper>);
 
 	expect(actual).toMatchSnapshot();
 });
