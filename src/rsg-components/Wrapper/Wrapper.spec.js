0d22e792d098772b96ce09842a38fa39466f695c
Jeremy Gayed <tizmagik@yahoo.com>
2017-10-24 01:18:55 -0400

    Add a test for good measure

--- src/rsg-components/Wrapper/Wrapper.spec.js
+++ src/rsg-components/Wrapper/Wrapper.spec.js
@@ -7,3 +7,14 @@ it('should render children', () => {
 
 	expect(actual).toMatchSnapshot();
 });
+
+it('should call onError handler when React invokes error handler', () => {
+	const onError = jest.fn();
+	const actual = shallow(<Wrapper onError={onError}>blah</Wrapper>);
+
+	// faux error
+	actual.instance().componentDidCatch('err');
+
+	expect(onError).toHaveBeenCalledTimes(1);
+	expect(onError).toHaveBeenCalledWith('err');
+});
