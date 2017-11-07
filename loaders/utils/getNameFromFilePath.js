7206ed8a9461f22f24699c816c2c7761b72db648
Jan-Felix <schwarz@mpango.de>
2017-11-07 15:38:50 +0100

    code cosmetics

--- loaders/utils/getNameFromFilePath.js
+++ loaders/utils/getNameFromFilePath.js
@@ -1,16 +1,12 @@
 'use strict';
 const path = require('path');
+const _ = require('lodash');
 
 module.exports = function getNameFromFilePath(filePath) {
 	let displayName = path.basename(filePath, path.extname(filePath));
 	if (displayName === 'index') {
-		const parts = path.dirname(filePath).split(path.sep);
-		displayName = parts[parts.length - 1];
+		displayName = path.basename(path.dirname(filePath));
 	}
 
-	return displayName
-		.charAt(0)
-		.toUpperCase()
-		.concat(displayName.slice(1))
-		.replace(/-([a-z])/, (_, match) => match.toUpperCase());
+	return _.upperFirst(_.camelCase(displayName));
 };
