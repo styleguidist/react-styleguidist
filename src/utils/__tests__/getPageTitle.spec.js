b4ce2f951e5e49689cf9f9b6beb1764d32eca106
Artem Sapegin <artem@sapegin.ru>
2017-11-08 11:52:44 +0100

    Test: Add tests for getPageTitle

---
+++ src/utils/__tests__/getPageTitle.spec.js
@@ -0,0 +1,28 @@
+import getPageTitle from '../getPageTitle';
+
+const baseTitle = 'Styleguide';
+
+describe('getPageTitle', () => {
+	it('should return style guide title for the all view', () => {
+		const result = getPageTitle([], baseTitle, 'all');
+		expect(result).toBe(baseTitle);
+	});
+
+	it('should return component name for component isolation mode', () => {
+		const name = 'Component';
+		const result = getPageTitle([{ components: [{ name }] }], baseTitle, 'component');
+		expect(result).toMatch(name);
+	});
+
+	it('should return component name for example isolation mode', () => {
+		const name = 'Component';
+		const result = getPageTitle([{ components: [{ name }] }], baseTitle, 'example');
+		expect(result).toMatch(name);
+	});
+
+	it('should return section name for section isolation mode', () => {
+		const name = 'Section';
+		const result = getPageTitle([{ name }], baseTitle, 'section');
+		expect(result).toMatch(name);
+	});
+});
