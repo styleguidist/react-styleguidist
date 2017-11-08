f654a01824836d34efacd6314a9d3f1b8907cd79
Artem Sapegin <artem@sapegin.ru>
2017-11-08 12:12:23 +0100

    Test: Add test for getRouteData, update snapshots

--- src/utils/getRouteData.js
+++ src/utils/getRouteData.js
@@ -11,9 +11,9 @@ import {
 /**
  * Return sections / components / examples to show on a screen according to a current route.
  *
- * /: show all sections and components.
- * /#!/Button: show only Button section or Button component
- * /#!/Button/1: show only the second example (index 1) of Button component
+ * Default: show all sections and components.
+ * #!/Button: show only Button section or Button component
+ * #!/Button/1: show only the second example (index 1) of Button component
  *
  * @param {object} allSections
  * @param {string} hash
