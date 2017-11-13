707404fa6ecdea8f4b9dad116c4a79f6260164b2
Artem Sapegin <artem@sapegin.ru>
2017-11-13 09:16:12 +0100

    Fix: Tweak text

--- scripts/schemas/config.js
+++ scripts/schemas/config.js
@@ -93,7 +93,7 @@ module.exports = {
 	highlightTheme: {
 		type: 'string',
 		default: 'base16-light',
-		deprecated: 'Please use the theme option in editorConfig',
+		deprecated: 'Use the theme property in the editorConfig option instead',
 	},
 	editorConfig: {
 		type: 'object',
