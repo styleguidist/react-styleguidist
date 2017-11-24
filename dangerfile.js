49da1c40b0928772128c7996e642594b386d40a7
Artem Sapegin <artem@sapegin.ru>
2017-11-24 19:50:42 +0100

    Chore: Warn on PR with lock file changes

--- dangerfile.js
+++ dangerfile.js
@@ -2,8 +2,15 @@ import { danger, warn } from 'danger';
 
 const packageChanged = danger.git.modified_files.includes('package.json');
 const lockfileChanged = danger.git.modified_files.includes('package-lock.json');
-if (packageChanged && !lockfileChanged) {
-	warn(`Changes were made to package.json, but not to package-lock.json.
 
-Perhaps you need to run \`npm install\` and commit changes in package-lock.json.. Make sure you’re using npm 5+.`);
+if (packageChanged && !lockfileChanged) {
+	warn(`Changes were made to \`package.json\`, but not to \`package-lock.json\`.
+
+Perhaps you need to run \`npm install\` and commit changes in package-lock.json. Make sure you’re using npm 5+.`);
+}
+
+if (!packageChanged && lockfileChanged) {
+	warn(`Changes were made to \`package-lock.json\`, but not to \`package.json\`.
+
+Please remove \`package-lock.json\` changes from your pull request. Try to run \`git checkout master -- package-lock.json\` and commit changes.`);
 }
