34a925a3f0587bdec3c6d231d44909f89325cf34
Fabian Mueller <mueller.fabian@gmail.com>
2018-01-23 09:15:02 +0100

    Revert "Sort properties by 'required' and 'name' attributes"
    This reverts commit 182fc89bfd1aca5269cdbe1e2a453e26d4aa6d58.

--- src/rsg-components/Props/PropsRenderer.js
+++ src/rsg-components/Props/PropsRenderer.js
@@ -12,6 +12,7 @@ import Type from 'rsg-components/Type';
 import Text from 'rsg-components/Text';
 import Para from 'rsg-components/Para';
 import Table from 'rsg-components/Table';
+import map from 'lodash/map';
 import { unquote, getType, showSpaces } from './util';
 
 function renderType(type) {
@@ -205,14 +206,7 @@ export function getRowKey(row) {
 }
 
 export function propsToArray(props) {
-	const propNames = Object.keys(props);
-	const requiredPropNames = propNames.filter(propName => props[propName].required).sort();
-	const optionalPropNames = propNames.filter(propName => !props[propName].required).sort();
-	const sortedPropNames = requiredPropNames.concat(optionalPropNames);
-	return sortedPropNames.map(name => ({
-		...props[name],
-		name,
-	}));
+	return map(props, (prop, name) => ({ ...prop, name }));
 }
 
 export const columns = [
