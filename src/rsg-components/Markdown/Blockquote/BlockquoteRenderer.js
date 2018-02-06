87e61df9c2585771e3d96515433ebde182d15ebe
Ryan Oglesby <ryan.oglesby08@gmail.com>
2018-02-05 20:08:56 -0500

    Remove Para styles from Markdown Blockquote component. Just copy them.

--- src/rsg-components/Markdown/Blockquote/BlockquoteRenderer.js
+++ src/rsg-components/Markdown/Blockquote/BlockquoteRenderer.js
@@ -2,14 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Styled from 'rsg-components/Styled';
-import { styles as paraStyles } from 'rsg-components/Para';
 
 const styles = ({ space, color, fontSize, fontFamily }) => ({
 	blockquote: {
-		...paraStyles({ space, color, fontFamily }).para,
-		fontSize: fontSize.base,
 		margin: [[space[2], space[4]]],
 		padding: 0,
+		color: color.base,
+		fontFamily: fontFamily.base,
+		fontSize: fontSize.base,
+		lineHeight: 1.5,
 	},
 });
 
