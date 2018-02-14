4ddf9502ac097395fc5a0f3cad97b6de8918d9f0
Terry Smith <tsmith@getchalk.com>
2018-02-14 15:26:05 -0400

    Removed tests

--- examples/react-native/src/Root.test.js
+++
@@ -1,9 +0,0 @@
-import renderer from 'react-test-renderer';
-
-import React from 'react';
-import Root from './Root';
-
-it('renders without crashing', () => {
-	const rendered = renderer.create(<Root />).toJSON();
-	expect(rendered).toBeTruthy();
-});
