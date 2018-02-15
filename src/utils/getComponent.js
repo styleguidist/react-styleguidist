9937186aeb72bf5d837ed9832f0014bbb1ce5559
Marc <marc@lodgify.com>
2018-02-15 17:21:29 +0100

    Add case for CommonJS module

--- src/utils/getComponent.js
+++ src/utils/getComponent.js
@@ -7,12 +7,15 @@
  * @return {function|object}
  */
 export default function getComponent(module, name) {
-	if (!module.default) {
-		const moduleKeys = Object.keys(module);
-		if (moduleKeys.length === 1) {
-			return module[moduleKeys[0]];
-		}
-		return module[name] || module;
+	if (module.default) {
+		return module.default;
 	}
-	return module.default;
+	if (!module.__esModule && typeof module === 'function') {
+		return module;
+	}
+	const moduleKeys = Object.keys(module);
+	if (moduleKeys.length === 1) {
+		return module[moduleKeys[0]];
+	}
+	return module[name] || module;
 }
