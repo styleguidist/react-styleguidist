90b998fdf25196ece76c1a51b1876dfa3eb40e7c
Marc <marc@lodgify.com>
2018-02-20 10:05:13 +0100

    Prettify comment blocks

--- src/utils/getComponent.js
+++ src/utils/getComponent.js
@@ -11,19 +11,23 @@ export default function getComponent(module, name) {
 	//
 	// If the module defines a default export, return that
 	// e.g.
+	//
 	// ```
 	// export default function Component() { ... }
 	// ```
+	//
 	if (module.default) {
 		return module.default;
 	}
 
 	// If it is a CommonJS module which exports a function, return that
 	// e.g.
+	//
 	// ```
 	// function Component() { ... }
 	// module.exports = Component;
 	// ```
+	//
 	if (!module.__esModule && typeof module === 'function') {
 		return module;
 	}
@@ -31,9 +35,11 @@ export default function getComponent(module, name) {
 
 	// If the module exports just one named export, return that
 	// e.g.
+	//
 	// ```
 	// export function Component() { ... }
 	// ```
+	//
 	if (moduleKeys.length === 1) {
 		return module[moduleKeys[0]];
 	}
@@ -41,6 +47,7 @@ export default function getComponent(module, name) {
 	// If the module exports a named export with the same name as the
 	// understood Component identifier, return that
 	// e.g.
+	//
 	// ```
 	// // /component.js
 	// export function someUtil() { ... }
@@ -48,5 +55,6 @@ export default function getComponent(module, name) {
 	// ```
 	//
 	// Else return the module itself
+	//
 	return module[name] || module;
 }
