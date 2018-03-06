db7d67d9c065a23b1fd09671d67be26cb64392b9
Gleb Kost <glebkost.dev@gmail.com>
2018-03-06 15:43:05 +0100

    feat: change template container id to prevent clashes
    * use 'rsg-root' id insted of 'app' on the main container
    * print deprecation warning to the console if 'app' id is used in custom template

--- src/index.js
+++ src/index.js
@@ -11,7 +11,17 @@ let codeRevision = 0;
 const render = () => {
 	// eslint-disable-next-line import/no-unresolved
 	const styleguide = require('!!../loaders/styleguide-loader!./index.js');
-	ReactDOM.render(renderStyleguide(styleguide, codeRevision), document.getElementById('app'));
+	let containerId = 'rsg-root';
+
+	if (document.getElementById('app')) {
+		// eslint-disable-next-line no-console
+		console.warn(
+			"The use of 'app' element id in the template is deprecated. Please, update your template file to use 'rsg-root' as the container id."
+		);
+		containerId = 'app';
+	}
+
+	ReactDOM.render(renderStyleguide(styleguide, codeRevision), document.getElementById(containerId));
 };
 
 window.addEventListener('hashchange', render);
