ef6329cd00920f872a89abb6b392ab49b85152f9
Gleb Kost <glebkost.dev@gmail.com>
2018-03-07 15:24:09 +0100

    update ribbon tests

--- src/rsg-components/Ribbon/Ribbon.spec.js
+++ src/rsg-components/Ribbon/Ribbon.spec.js
@@ -1,34 +1,36 @@
 import React from 'react';
+import Ribbon from './Ribbon';
 import { RibbonRenderer, styles } from './RibbonRenderer';
 
 const props = {
 	classes: classes(styles),
 };
 
-it('should render ribbon with url', () => {
-	const actual = shallow(<RibbonRenderer {...props} url="http://example.com" />);
+describe('Ribbon', () => {
+	it('should render ribbon if the ribbon is present in the config', () => {
+		const actual = shallow(<Ribbon />, { context: { config: { ribbon: { url: 'foo.bar' } } } });
 
-	expect(actual).toMatchSnapshot();
+		expect(actual).toMatchSnapshot();
+	});
+
+	it('should return null if the ribbon is not present in the config', () => {
+		const actual = shallow(<Ribbon />, { context: { config: {} } });
+
+		expect(actual.type()).toBeNull();
+	});
 });
+describe('RibbonRenderer', () => {
+	it('should render ribbon with url', () => {
+		const actual = shallow(<RibbonRenderer {...props} url="http://example.com" />);
 
-it('should render ribbon with a text', () => {
-	const actual = shallow(
-		<RibbonRenderer {...props} url="http://example.com" text="Share the repo" />
-	);
+		expect(actual).toMatchSnapshot();
+	});
 
-	expect(actual).toMatchSnapshot();
-});
+	it('should render ribbon with a text', () => {
+		const actual = shallow(
+			<RibbonRenderer {...props} url="http://example.com" text="Share the repo" />
+		);
 
-it('should render ribbon with custom background and color', () => {
-	const actual = shallow(
-		<RibbonRenderer
-			{...props}
-			url="http://example.com"
-			text="Share the repo"
-			background="#cdcdcd"
-			color="#aa00bb"
-		/>
-	);
-
-	expect(actual).toMatchSnapshot();
+		expect(actual).toMatchSnapshot();
+	});
 });
