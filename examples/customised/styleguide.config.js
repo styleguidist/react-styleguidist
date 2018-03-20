9916f14134956a3fd62162e1e3d7adfbae014fc2
Rob Levin <roblevintennis@gmail.com>
2018-03-20 10:58:18 -0700

    Add some comments

--- examples/customised/styleguide.config.js
+++ examples/customised/styleguide.config.js
@@ -35,7 +35,9 @@ module.exports = {
 		return `import { ${name} } from 'my-awesome-library';`;
 	},
 
-	// Example of overriding the CLI message in local development
+	// Example of overriding the CLI message in local development.
+	// Uncomment/edit the following `serverHost` entry to see in output
+	// serverHost: 'your-domain',
 	printServerInstructions(config) {
 		// eslint-disable-next-line no-console
 		console.log(`'View your styleguide at: http://${config.serverHost}`);
