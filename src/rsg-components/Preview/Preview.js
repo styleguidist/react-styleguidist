d297a99b4b8092c816619cc5ca4806285aacf312
Artem Sapegin <artem@sapegin.ru>
2018-03-31 20:42:59 +0200

    Refactor: Simplify Fragment wrapping

--- src/rsg-components/Preview/Preview.js
+++ src/rsg-components/Preview/Preview.js
@@ -9,12 +9,10 @@ import splitExampleCode from '../../utils/splitExampleCode';
 /* eslint-disable no-invalid-this, react/no-multi-comp */
 
 const Fragment = React.Fragment ? React.Fragment : 'div';
+const FragmentTag = React.Fragment ? 'React.Fragment' : "'div'";
 
 const compileCode = (code, config) => transform(code, config).code;
-const wrapCodeInFragment = code =>
-	`var Fragment__ = ${
-		React.Fragment ? 'React.Fragment' : "'div'"
-	}; <Fragment__>${code}</Fragment__>;`;
+const wrapCodeInFragment = code => `<${FragmentTag}>${code}</${FragmentTag}>;`;
 
 // Wrap everything in a React component to leverage the state management
 // of this component
