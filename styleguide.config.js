44e775e08eeb916ee8ce8c8a924dbedebce09e11
Allison Zhao <gryffinzhao@gmail.com>
2018-04-04 11:10:57 -0400

    Add styleguide.config.js

---
+++ styleguide.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	components: 'src/rsg-components/**/[A-Z]*.js',
+	defaultExample: true,
+	webpackConfig: {
+		module: {
+			rules: [
+				{
+					test: /\.jsx?$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader',
+				},
+				{
+					test: /\.css$/,
+					loader: 'style-loader!css-loader',
+				},
+			],
+		},
+	},
+};
