5489de35ba968e5ac4a4f879b1bbf9051450ff26
Gleb Kost <glebkost.dev@gmail.com>
2018-04-10 22:29:07 +0200

    bring tests up to speed with new sgDisplayName

--- src/rsg-components/ReactComponent/ReactComponent.spec.js
+++ src/rsg-components/ReactComponent/ReactComponent.spec.js
@@ -17,6 +17,7 @@ const options = {
 
 const component = {
 	name: 'Foo',
+	sgDisplayName: 'Foo',
 	slug: 'foo',
 	pathLine: 'foo/bar.js',
 	props: {
@@ -28,6 +29,7 @@ const component = {
 };
 const componentWithEverything = {
 	name: 'Foo',
+	sgDisplayName: 'Foo',
 	slug: 'foo',
 	pathLine: 'foo/bar.js',
 	props: {
