285e26dbca6d5dda2cca0a9b27a956af7cb920c9
Gleb Kost <glebkost.dev@gmail.com>
2018-04-12 13:30:16 +0200

    change componentsList to use visibleName instead of name

--- src/rsg-components/ComponentsList/ComponentsList.spec.js
+++ src/rsg-components/ComponentsList/ComponentsList.spec.js
@@ -5,10 +5,12 @@ import { ComponentsListRenderer } from './ComponentsListRenderer';
 it('should set the correct href for items', () => {
 	const components = [
 		{
+			visibleName: 'Button',
 			name: 'Button',
 			slug: 'button',
 		},
 		{
+			visibleName: 'Input',
 			name: 'Input',
 			slug: 'input',
 		},
@@ -21,10 +23,12 @@ it('should set the correct href for items', () => {
 it('should set the correct href for items when isolated links should be used', () => {
 	const components = [
 		{
+			visibleName: 'Button',
 			name: 'Button',
 			slug: 'button',
 		},
 		{
+			visibleName: 'Input',
 			name: 'Input',
 			slug: 'input',
 		},
@@ -37,12 +41,12 @@ it('should set the correct href for items when isolated links should be used', (
 it('should render sections with nested components', () => {
 	const components = [
 		{
-			name: 'Button',
+			visibleName: 'Button',
 			slug: 'button',
 			href: '#button',
 		},
 		{
-			name: 'Input',
+			visibleName: 'Input',
 			slug: 'input',
 			href: '#input',
 		},
@@ -58,10 +62,10 @@ it('should return null when the list is empty', () => {
 	expect(actual.getElement()).toBe(null);
 });
 
-it('should ignore items without name', () => {
+it('should ignore items without visibleName', () => {
 	const components = [
 		{
-			name: 'Button',
+			visibleName: 'Button',
 			slug: 'button',
 			href: '#button',
 		},
