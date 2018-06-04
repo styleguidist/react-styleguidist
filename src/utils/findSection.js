4bdfed2fd635a782ca9269935f668d973227a66a
rafaesc <rafaesc92@gmail.com>
2018-06-04 14:34:28 -0500

    Fix: Added comment

--- src/utils/findSection.js
+++ src/utils/findSection.js
@@ -8,6 +8,7 @@ import find from 'lodash/find';
  * @return {object}
  */
 export default function findSection(sections, name) {
+	// We're using Lodash because IE11 doesn't support Array.find.
 	let found = find(sections, { name });
 	if (found) {
 		return found;
