340ec14cc4797f8c9b58c768069502180d377c76
Maxim Sukharev <maxim@sourced.tech>
2018-07-12 11:02:18 +0200

    Isolate CodeMirror styles

--- src/rsg-components/Editor/Editor.spec.js
+++ src/rsg-components/Editor/Editor.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { EditorLoaderRenderer } from './EditorLoaderRenderer';
-import Editor from './Editor';
+import { Editor } from './Editor';
 
 const code = '<button>MyAwesomeCode</button>';
 const newCode = '<button>MyNewAwesomeCode</button>';
@@ -32,7 +32,7 @@ describe('EditorLoaderRenderer', () => {
 describe('Editor', () => {
 	it('should renderer and editor', () => {
 		const actual = shallow(
-			<Editor code={code} editorConfig={{ readOnly: true, mode: 'text/html' }} />,
+			<Editor classes={{}} code={code} editorConfig={{ readOnly: true, mode: 'text/html' }} />,
 			options
 		);
 
@@ -41,7 +41,7 @@ describe('Editor', () => {
 
 	it('should update code with debounce', done => {
 		const onChange = jest.fn();
-		const actual = mount(<Editor code={code} onChange={onChange} />, options);
+		const actual = mount(<Editor classes={{}} code={code} onChange={onChange} />, options);
 
 		expect(actual.text()).toMatch(code);
 
@@ -59,14 +59,17 @@ describe('Editor', () => {
 	});
 
 	it('should not update if not read only', () => {
-		const actual = shallow(<Editor code={code} />, options);
+		const actual = shallow(<Editor classes={{}} code={code} />, options);
 
 		const shouldUpdate = actual.instance().shouldComponentUpdate({ code: newCode });
 		expect(shouldUpdate).toBe(false);
 	});
 
 	it('should update if read only and code has changed', () => {
-		const actual = shallow(<Editor code={code} editorConfig={{ readOnly: true }} />, options);
+		const actual = shallow(
+			<Editor classes={{}} code={code} editorConfig={{ readOnly: true }} />,
+			options
+		);
 
 		const shouldUpdate = actual
 			.instance()
