68328d024b6a563050c127585bb994e8745e17c2
Artem Sapegin <asapegin@wayfair.com>
2018-08-08 11:18:39 +0200

    Fix: Fix section name regression
    Fixes #1078

--- src/utils/__tests__/processSections.spec.js
+++ src/utils/__tests__/processSections.spec.js
@@ -5,6 +5,7 @@ const sections = deepfreeze([
 	{
 		sections: [
 			{
+				name: 'Components',
 				components: [
 					{
 						props: {
@@ -22,5 +23,10 @@ describe('processSections', () => {
 	it('should recursively process all sections and components', () => {
 		const result = processSections(sections);
 		expect(result[0].sections[0].components[0].name).toBe('Button');
+	});
+
+	it('should set visibleName property on each section', () => {
+		const result = processSections(sections);
+		expect(result[0].sections[0].visibleName).toBe('Components');
 	});
 });
