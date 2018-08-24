8baa6e71f6a87c5ae058ae08206cf23632f7b370
Agata Krzywda <agatta1504@interia.pl>
2018-08-24 15:37:43 +0100

    update snapshot test

--- src/rsg-components/Markdown/Details/Details.spec.js
+++ src/rsg-components/Markdown/Details/Details.spec.js
@@ -4,9 +4,7 @@ import Details from './index';
 
 describe('Markdown Details', () => {
 	it('should render a Details', () => {
-		const actual = render(
-			<Details tag="This is a summary element">This is Details-formatted text.</Details>
-		);
+		const actual = render(<Details />);
 
 		expect(actual).toMatchSnapshot();
 	});
