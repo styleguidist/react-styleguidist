19f7f03c572e0e448512d32164aee6ba93a05ad8
Artem Sapegin <artem@sapegin.ru>
2018-08-31 09:51:02 +0200

    Fix: Add pointer cursor on <summary> element

--- src/client/rsg-components/Markdown/Details/DetailsRenderer.js
+++ src/client/rsg-components/Markdown/Details/DetailsRenderer.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import Styled from 'rsg-components/Styled';
 
 const styles = ({ space, color, fontSize, fontFamily }) => ({
 	details: {
+		marginBottom: space[2],
 		fontFamily: fontFamily.base,
 		fontSize: fontSize.base,
 		color: color.base,
-		marginBottom: space[2],
 	},
 });
 
 export function DetailsRenderer({ classes, children }) {
 	return <details className={classes.details}>{children}</details>;
 }
+
 DetailsRenderer.propTypes = {
 	classes: PropTypes.object.isRequired,
 	children: PropTypes.node.isRequired,
