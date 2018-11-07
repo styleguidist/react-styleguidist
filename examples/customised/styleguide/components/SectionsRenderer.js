5c3502978d7084fd83f53c8f0b53cf70651f0878
Artem Sapegin <asapegin@wayfair.com>
2018-11-07 09:15:19 +0100

    Docs: Fix customised example

--- examples/customised/styleguide/components/SectionsRenderer.js
+++ examples/customised/styleguide/components/SectionsRenderer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Styled from 'rsg-components/Styled';
 import Heading from 'rsg-components/Heading';
 // Import default implementation from react-styleguidist using the full path
-import DefaultSectionsRenderer from 'react-styleguidist/lib/rsg-components/Sections/SectionsRenderer';
+import DefaultSectionsRenderer from 'react-styleguidist/lib/client/rsg-components/Sections/SectionsRenderer';
 
 const styles = ({ fontFamily, space }) => ({
 	headingSpacer: {
