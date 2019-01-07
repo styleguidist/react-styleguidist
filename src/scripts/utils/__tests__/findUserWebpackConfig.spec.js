992fe8ab8a1d61f581fdae7530036547eb833f68
Olivier Terral <olivier.terral@geops.de>
2019-01-07 12:24:51 +0100

    Add tests

--- src/scripts/utils/__tests__/findUserWebpackConfig.spec.js
+++ src/scripts/utils/__tests__/findUserWebpackConfig.spec.js
@@ -3,9 +3,19 @@ import findUserWebpackConfig from '../findUserWebpackConfig';
 const cwd = process.cwd();
 afterEach(() => process.chdir(cwd));
 
-it('should return path to Create React App Webpack config', () => {
+it('should return path to Create React App Webpack dev config', () => {
 	const result = findUserWebpackConfig(a => a);
-	expect(result).toMatch(/^react-scripts\//);
+	expect(result).toMatch(/^react-scripts\/config\/webpack\.config\.dev/);
+});
+
+it('should return path to Create React App Webpack config', () => {
+	const result = findUserWebpackConfig(a => {
+		if (/webpack\.config\.dev/.test(a)) {
+			throw window.Exception();
+		}
+		return a;
+	});
+	expect(result).toMatch(/^react-scripts\/config\/webpack\.config/);
 });
 
 it('should return an absolute path to user Webpack config located in project root folder', () => {
