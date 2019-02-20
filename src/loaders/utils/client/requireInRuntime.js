75c393be1493cd0170284f4e64dd42a21e9943a3
Zihua Wu <qqbbnease1004@126.com>
2019-02-20 16:29:32 +0800

    Chore: Fix typos in comments (#1275)

--- src/loaders/utils/client/requireInRuntime.js
+++ src/loaders/utils/client/requireInRuntime.js
@@ -5,7 +5,7 @@ type RequireMap = { [filepath: string]: Module };
 
 /**
  * Return module from a given map (like {react: require('react')}) or throw.
- * We alllow to require modules only from Markdown examples (won’t work dinamically becasue we need to know all required
+ * We allow to require modules only from Markdown examples (won’t work dynamically becasue we need to know all required
  * modules in advance to be able to bundle them with the code).
  */
 export default function requireInRuntime(requireMap: RequireMap, filepath: string): Module {
