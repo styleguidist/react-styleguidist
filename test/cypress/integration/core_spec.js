b599b0fc1dd9f5b9c726b83c68abea1a3501afe9
Doug MacKenzie <douglmackenzie@gmail.com>
2019-03-04 15:06:51 +1100

    Test that multiple components show again after exiting isolated mode

--- test/cypress/integration/core_spec.js
+++ test/cypress/integration/core_spec.js
@@ -19,4 +19,12 @@ describe('Styleguidist core', () => {
 	it('hides the sidebar in isolated mode', () => {
 		cy.get('[class^=rsg--sidebar]').should('not.exist');
 	});
+
+	it('returns to showing multiple components after exiting isolated mode', () => {
+		cy.get('a[aria-label="Show all components"]')
+			.first()
+			.click();
+
+		cy.get('[id$=container]').should('have.length.above', 1);
+	});
 });
