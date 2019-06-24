6b54bc1206eadc944bb77e7aefeda3cc60cab32a
Andrey Medvedev <andr.medv.spb@gmail.com>
2019-06-24 21:29:10 +0300

    Make more clear variable name for file name

--- src/loaders/utils/getNameFromFilePath.js
+++ src/loaders/utils/getNameFromFilePath.js
@@ -1,17 +1,17 @@
 const path = require('path');
 const { startCase } = require('lodash');
 
-function transformDisplayName(displayName) {
-	// ex: your-buttonTS => Your Button TS => YourButtonTS
-	// ex: your_button--TS => Your Button TS => YourButtonTS
+function transformFileNameToDisplayName(displayName) {
+	// ex: your-buttonTS -> Your Button TS -> YourButtonTS
+	// ex: your_button--TS -> Your Button TS -> YourButtonTS
 	return startCase(displayName).replace(/\s/g, '');
 }
 
 module.exports = function getNameFromFilePath(filePath) {
-	let displayName = path.basename(filePath, path.extname(filePath));
-	if (displayName === 'index') {
-		displayName = path.basename(path.dirname(filePath));
+	let fileName = path.basename(filePath, path.extname(filePath));
+	if (fileName === 'index') {
+		fileName = path.basename(path.dirname(filePath));
 	}
 
-	return transformDisplayName(displayName);
+	return transformFileNameToDisplayName(fileName);
 };
