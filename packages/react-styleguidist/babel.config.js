0c4b12b450088d12734fce6c252368e75d997315
elevatebart <ledouxb@me.com>
2019-06-25 10:35:37 -0500

    Ci: make bebel  ok with node 6

--- packages/react-styleguidist/babel.config.js
+++ packages/react-styleguidist/babel.config.js
@@ -1,23 +1,24 @@
-module.exports = {
-	...require('../../babel.config'),
-	overrides: [
-		{
-			include: ['src/bin', 'src/loaders', 'src/scripts', 'src/share'],
-			exclude: ['src/loaders/utils/client'],
-			presets: [
-				[
-					'@babel/env',
-					{
-						loose: true,
-						modules: 'commonjs',
-						useBuiltIns: 'usage',
-						corejs: 3,
-						targets: {
-							node: '6.4',
-						},
+const mainConfig = require('../../babel.config');
+
+mainConfig.overrides = [
+	{
+		include: ['src/bin', 'src/loaders', 'src/scripts', 'src/share'],
+		exclude: ['src/loaders/utils/client'],
+		presets: [
+			[
+				'@babel/env',
+				{
+					loose: true,
+					modules: 'commonjs',
+					useBuiltIns: 'usage',
+					corejs: 3,
+					targets: {
+						node: '6.4',
 					},
-				],
+				},
 			],
-		},
-	],
-};
+		],
+	},
+];
+
+module.exports = mainConfig;
