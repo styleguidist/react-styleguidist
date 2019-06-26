e52c9d111c1d8f976b69983d83e8a3980c9c1258
elevatebart <ledouxb@me.com>
2019-06-25 19:17:29 -0500

    Ci: Fix dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -2,16 +2,18 @@
 import { danger, warn } from 'danger';
 
 const packageChanged = danger.git.modified_files.includes('package.json');
-const lockfileChanged = danger.git.modified_files.includes('package-lock.json');
+const packagesChanged =
+	packageChanged || danger.git.modified_files.includes('packages/react-styleguidist/package.json');
+const lockfileChanged = danger.git.modified_files.includes('yarn.lock');
 
-if (packageChanged && !lockfileChanged) {
-	warn(`Changes were made to \`package.json\`, but not to \`package-lock.json\`.
+if (packagesChanged && !lockfileChanged) {
+	warn(`Changes were made to \`package.json\`, but not to \`yarn.lock\`.
 
 If you’ve changed any dependencies (added, removed or updated any packages), please run \`npm install\` and commit changes in package-lock.json file. Make sure you’re using npm 5+.`);
 }
 
-if (!packageChanged && lockfileChanged) {
-	warn(`Changes were made to \`package-lock.json\`, but not to \`package.json\`.
+if (!packagesChanged && lockfileChanged) {
+	warn(`Changes were made to \`yarn.lock\`, but not to \`package.json\`.
 
-Please remove \`package-lock.json\` changes from your pull request. Try to run \`git checkout master -- package-lock.json\` and commit changes.`);
+Please remove \`yarn.lock\` changes from your pull request. Try to run \`git checkout master -- package-lock.json\` and commit changes.`);
 }
