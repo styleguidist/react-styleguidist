83b41d47ce0a9512f4fd4defc53315881d71ad18
elevatebart <ledouxb@me.com>
2019-11-06 09:01:54 -0600

    first working jsx

--- src/client/rsg-components/Styled/Styled.spec.tsx
+++ src/client/rsg-components/Styled/Styled.spec.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from '@testing-library/react';
-import Styled from './Styled';
+import Styled, { JssInjectedProps } from './Styled';
 import Context from '../Context';
 
 const context = {
@@ -10,7 +10,7 @@ const context = {
 	},
 };
 
-const Provider = props => <Context.Provider value={context} {...props} />;
+const Provider = (props: any) => <Context.Provider value={context} {...props} />;
 
 /* eslint-disable react/prefer-stateless-function, react/prop-types */
 
@@ -20,8 +20,12 @@ const styles = () => ({
 	},
 });
 
-class TestRenderer extends Component {
-	render() {
+interface MockProps extends JssInjectedProps {
+	testId?: string;
+}
+
+class TestRenderer extends Component<MockProps> {
+	public render() {
 		return <div className={this.props.classes.foo} data-testid={this.props.testId} />;
 	}
 }
@@ -32,7 +36,7 @@ test('should set displayName', () => {
 });
 
 test('should wrap a component and pass props and classes', () => {
-	const WrappedComponent = Styled(styles)(TestRenderer);
+	const WrappedComponent = Styled<MockProps>(styles)(TestRenderer);
 	const { getByTestId } = render(
 		<Provider>
 			<WrappedComponent testId="element" />
--- src/client/rsg-components/Styled/Styled.tsx
+++ src/client/rsg-components/Styled/Styled.tsx
@@ -1,25 +1,34 @@
 import React, { Component, ComponentType, ComponentClass } from 'react';
-import { Styles } from 'jss';
+import { Styles, StyleSheet, Classes } from 'jss';
 import Context from 'rsg-components/Context';
 import createStyleSheet from '../../styles/createStyleSheet';
 
-export default (styles: Styles) => (WrappedComponent: ComponentClass):ComponentClass => {
-	const componentName = WrappedComponent.name.replace(/Renderer$/, '');
-	return class extends Component {
-		public static displayName = `Styled(${componentName})`;
-		public static contextType = Context;
-		public constructor(props, context) {
-			super(props, context);
-			this.sheet = createStyleSheet(styles, context.config || {}, componentName);
-			this.sheet.update(props).attach();
-		}
+export interface JssInjectedProps {
+	classes: Classes;
+}
 
-		public componentDidUpdate(nextProps) {
-			this.sheet.update(nextProps);
-		}
+export default function StyleHOC<P extends JssInjectedProps>(
+	styles: Styles
+): (WrappedComponent: ComponentClass<P>) => ComponentType<Omit<P, keyof JssInjectedProps>> {
+	return (WrappedComponent: ComponentClass<P>) => {
+		const componentName = WrappedComponent.name.replace(/Renderer$/, '');
+		return class extends Component<Omit<P, keyof JssInjectedProps>> {
+			public static displayName = `Styled(${componentName})`;
+			public static contextType = Context;
+			public sheet: StyleSheet;
+			public constructor(props: Omit<P, keyof JssInjectedProps>, context: any) {
+				super(props, context);
+				this.sheet = createStyleSheet(styles, context.config || {}, componentName);
+				this.sheet.update(props).attach();
+			}
 
-		public render() {
-			return <WrappedComponent {...this.props} classes={this.sheet.classes} />;
-		}
+			public componentDidUpdate(nextProps: P) {
+				this.sheet.update(nextProps);
+			}
+
+			public render() {
+				return <WrappedComponent {...({ ...this.props, classes: this.sheet.classes } as P)} />;
+			}
+		};
 	};
-};
+}
