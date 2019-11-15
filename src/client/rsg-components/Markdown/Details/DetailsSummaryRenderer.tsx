6fd7958e2b50bd8f347ef89017efb299e9cd0529
Barthelemy Ledoux <bart.ledoux@enablon.com>
2019-11-15 16:26:03 -0600

    make details ts

---
+++ src/client/rsg-components/Markdown/Details/DetailsSummaryRenderer.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Styled, { JssInjectedProps, Theme } from 'rsg-components/Styled';
+
+const styles = ({ space, color, fontSize, fontFamily }: Theme) => ({
+	summary: {
+		marginBottom: space[1],
+		fontFamily: fontFamily.base,
+		fontSize: fontSize.base,
+		color: color.base,
+		cursor: 'pointer',
+		'&:focus': {
+			isolate: false,
+			outline: [[1, 'dotted', color.linkHover]],
+			outlineOffset: 2,
+		},
+	},
+});
+
+interface DetailsSummaryProps extends JssInjectedProps {
+	children: React.ReactNode;
+}
+
+export const DetailsSummaryRenderer: React.FunctionComponent<DetailsSummaryProps> = ({
+	classes,
+	children,
+}) => {
+	return <summary className={classes.summary}>{children}</summary>;
+};
+
+DetailsSummaryRenderer.propTypes = {
+	classes: PropTypes.objectOf(PropTypes.string.isRequired).isRequired,
+	children: PropTypes.node.isRequired,
+};
+
+export default Styled<DetailsSummaryProps>(styles)(DetailsSummaryRenderer);
