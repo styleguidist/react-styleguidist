98f4def897993c3fa5f364e1e0d0828657d7e8b5
elevatebart <ledouxb@me.com>
2019-11-16 12:52:57 -0600

    Fix: remove style warning when compiling

--- src/client/rsg-components/Styled/index.ts
+++ src/client/rsg-components/Styled/index.ts
@@ -1,1 +1,2 @@
-export { default, JssInjectedProps, Theme } from 'rsg-components/Styled/Styled';
+export { default } from 'rsg-components/Styled/Styled';
+export * from 'rsg-components/Styled/Styled';
