233fe090f1ac3c2938375bcbbebd36caf22519ce
elevatebart <ledouxb@me.com>
2019-11-17 16:08:06 -0600

    better url comments

--- src/client/utils/getUrl.ts
+++ src/client/utils/getUrl.ts
@@ -25,10 +25,22 @@ function buildIsolatedOrNoChromeFragment({
 interface GetUrlOptions {
 	name: string;
 	slug: string;
+	/**
+	 * Example index
+	 */
 	example: number;
 	anchor: boolean;
+	/**
+	 * Isolated mode
+	 */
 	isolated: boolean;
+	/**
+	 * No chrome? (Can be combined with anchor or isolated)
+	 */
 	nochrome: boolean;
+	/**
+	 * Absolute URL? (Can be combined with other flags)
+	 */
 	absolute: boolean;
 	hashPath: string[];
 	id: boolean;
@@ -38,14 +50,8 @@ interface GetUrlOptions {
 /**
  * Get component / section URL.
  *
- * @param {string} $.name Name
- * @param {string} $.slug Slug
- * @param {number} $.example Example index
- * @param {boolean} $.anchor Anchor?
- * @param {boolean} $.isolated Isolated mode?
- * @param {boolean} $.nochrome No chrome? (Can be combined with anchor or isolated)
- * @param {boolean} $.absolute Absolute URL? (Can be combined with other flags)
- * @param {object} [location] Location object (will use current page location by default)
+ * @param {GetUrlOptions} options
+ * @param location Location object (will use current page location by default)
  * @return {string}
  */
 export default function getUrl(
@@ -70,7 +76,7 @@ export default function getUrl(
 		pathname: string;
 		hash: string;
 	} = window.location
-) {
+): string {
 	let url = pathname;
 
 	const currentHash = hash.indexOf('?') > -1 ? hash.substring(0, hash.indexOf('?')) : hash;
