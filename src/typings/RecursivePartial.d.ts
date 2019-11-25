7e86ba14a601b23f171e3efdb1c9085360effa33
elevatebart <ledouxb@me.com>
2019-11-25 08:28:09 -0600

    fix partial for functions

--- src/typings/RecursivePartial.d.ts
+++ src/typings/RecursivePartial.d.ts
@@ -9,6 +9,8 @@
 type RecursivePartial<T> = {
 	[P in keyof T]?: T[P] extends (infer U)[]
 		? RecursivePartial<U>[]
+		: T[P] extends Function
+		? T[P]
 		: T[P] extends object
 		? RecursivePartial<T[P]>
 		: T[P];
