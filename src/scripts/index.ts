afd56141327261ac0854b424728a3ebb00150677
elevatebart <ledouxb@me.com>
2019-11-27 07:52:34 -0600

    add comment about es5 export

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -11,6 +11,9 @@ import setupLogger from './logger';
 /**
  * Initialize Styleguide API.
  *
+ * NOTE: here we export an es5 module to avoid breaking the API
+ * TODO: in the next major version, use `export default` instead
+ *
  * @param {object} [config] Styleguidist config.
  * @returns {object} API.
  */
