c04511635c446bea2f9e5e69f25e8e2d9fe2fb39
elevatebart <ledouxb@me.com>
2019-12-07 14:23:19 -0600

    make example filenames more explicit

--- examples/themed/styleguide.config.js
+++ examples/themed/styleguide.config.js
@@ -10,8 +10,8 @@ module.exports = {
 	ribbon: {
 		url: 'https://github.com/styleguidist/react-styleguidist',
 	},
-	theme: path.resolve(__dirname, './theme.js'),
-	styles: path.resolve(__dirname, './styles.js'),
+	theme: path.resolve(__dirname, './styleguide.theme.js'),
+	styles: path.resolve(__dirname, './styleguide.styles.js'),
 	version,
 	webpackConfig: {
 		module: {
