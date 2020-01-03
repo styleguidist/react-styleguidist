58cf29d4cd1f15fe55bda5293cb9db53de4c00b6
Lazyuki <yukiito@uw.edu>
2020-01-03 03:31:02 -0500

    Breaking: Prefer default exports over named exports (#1504)

--- src/loaders/examples-loader.ts
+++ src/loaders/examples-loader.ts
@@ -81,16 +81,16 @@ export default function examplesLoader(this: Rsg.StyleguidistLoaderContext, sour
 				b.variableDeclaration('const', [
 					b.variableDeclarator(b.identifier(`${name}$0`), requireIt(requireRequest).toAST() as any),
 				]),
-				// const name = name$0[name] || name$0.default || name$0;
+				// const name = name$0.default || name$0[name] || name$0;
 				b.variableDeclaration('const', [
 					b.variableDeclarator(
 						b.identifier(name),
 						b.logicalExpression(
 							'||',
-							b.identifier(`${name}$0['${name}']`),
+							b.identifier(`${name}$0.default`),
 							b.logicalExpression(
 								'||',
-								b.identifier(`${name}$0.default`),
+								b.identifier(`${name}$0['${name}']`),
 								b.identifier(`${name}$0`)
 							)
 						)
