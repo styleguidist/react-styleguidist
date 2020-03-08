a433a9ef2086f7001926dfbb1c1cd0a43429b4c1
elevatebart <ledouxb@me.com>
2020-03-08 12:04:48 -0500

    add index for types

---
+++ src/typings/index.ts
@@ -0,0 +1,7 @@
+export * from './RsgComponent';
+export * from './RsgExample';
+export * from './RsgPropsObject';
+export * from './RsgRequireItResult';
+export * from './RsgSection';
+export * from './RsgStyleguidistConfig';
+export * from './RsgTheme';
