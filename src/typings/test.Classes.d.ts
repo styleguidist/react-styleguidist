ae2654c6ca339e02e1be0bcd597f7e11fd750254
elevatebart <ledouxb@me.com>
2020-03-10 15:38:24 -0500

    make class strongly typed again

--- src/typings/test.Classes.d.ts
+++ src/typings/test.Classes.d.ts
@@ -1,5 +1,9 @@
-/**
- * function used in react tests to generate
- * mocks of JSS Class names
- */
-declare const classes: (styles: (theme: any) => Record<string, any>) => Record<string, string>;
+import { Theme } from './RsgTheme';
+
+declare namespace global {
+	/**
+	 * function used in react tests to generate
+	 * mocks of JSS Class names
+	 */
+	function classes(styles: (theme: Theme) => Record<string, any>): Record<string, string>;
+}
