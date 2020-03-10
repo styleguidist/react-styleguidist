f49a3c96101b5dded7d7cc692d6d97dee93230f7
elevatebart <ledouxb@me.com>
2020-03-10 16:21:32 -0500

    fix typings

--- src/typings/test.Classes.d.ts
+++ src/typings/test.Classes.d.ts
@@ -1,9 +1,9 @@
 import { Theme } from './RsgTheme';
 
-declare namespace global {
+declare global {
 	/**
 	 * function used in react tests to generate
 	 * mocks of JSS Class names
 	 */
-	function classes(styles: (theme: Theme) => Record<string, any>): Record<string, string>;
+	const classes: (styles: (theme: Theme) => Record<string, any>) => Record<string, string>;
 }
