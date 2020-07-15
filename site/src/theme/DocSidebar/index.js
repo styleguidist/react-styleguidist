1ada32124493eb96f754556604d147159c1864b9
Artem Sapegin <artem@sapegin.ru>
2020-07-15 08:39:43 +0200

    docs: Update site ads on navigation

--- site/src/theme/DocSidebar/index.js
+++ site/src/theme/DocSidebar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import useScript from '@charlietango/use-script';
 import DocSidebarBase from '@theme-original/DocSidebar';
@@ -7,8 +7,17 @@ import styles from './styles.module.css';
 const SCRIPT_URL = 'https://media.ethicalads.io/media/client/ethicalads.min.js';
 const PUBLISHER_ID = 'react-styleguidist';
 
-function Extra() {
+function Extra({ path }) {
+	// Load the EthicalAds script
 	useScript(SCRIPT_URL);
+
+	// Update the ad on navigation
+	useEffect(() => {
+		if (window.ethicalads) {
+			window.ethicalads.load_placements();
+		}
+	}, [path]);
+
 	return (
 		<div className={styles.extra}>
 			<div data-ea-publisher={PUBLISHER_ID} data-ea-type="image" className="bordered"></div>
@@ -16,17 +25,22 @@ function Extra() {
 	);
 }
 
+Extra.propTypes = {
+	path: PropTypes.string.isRequired,
+};
+
 function DocSidebar({ showExtra, ...props }) {
 	return (
 		<div className={styles.sidebar}>
 			<DocSidebarBase {...props} />
-			{showExtra && <Extra />}
+			{showExtra && <Extra path={props.path} />}
 		</div>
 	);
 }
 
 DocSidebar.propTypes = {
 	showExtra: PropTypes.bool,
+	path: PropTypes.string.isRequired,
 };
 
 DocSidebar.defaultProps = {
