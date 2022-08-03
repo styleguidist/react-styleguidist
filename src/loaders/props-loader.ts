5deb26651f28a9d7b225d663eb267e33330fc932
Thomas Roest <thomas.roest@moxio.com>
2022-08-03 11:01:15 +0200

    fix: replace 'any' types in try catch with instance of Error check

--- src/loaders/props-loader.ts
+++ src/loaders/props-loader.ts
@@ -47,19 +47,21 @@ export default function (this: Rsg.StyleguidistLoaderContext, source: string) {
 			}
 			docs = docs[0];
 		}
-	} catch (err: any) {
-		const errorMessage = err.toString();
-		const componentPath = path.relative(process.cwd(), file);
-		const message =
-			errorMessage === `Error: ${ERROR_MISSING_DEFINITION}`
-				? `${componentPath} matches a pattern defined in “components” or “sections” options in your ` +
-				  'style guide config but doesn’t export a component.\n\n' +
-				  'It usually happens when using third-party libraries, see possible solutions here:\n' +
-				  `${consts.DOCS_THIRDPARTIES}`
-				: `Cannot parse ${componentPath}: ${err}\n\n` +
-				  'It usually means that react-docgen does not understand your source code, try to file an issue here:\n' +
-				  'https://github.com/reactjs/react-docgen/issues';
-		logger.warn(message);
+	} catch (err) {
+		if (err instanceof Error) {
+			const errorMessage = err.toString();
+			const componentPath = path.relative(process.cwd(), file);
+			const message =
+				errorMessage === `Error: ${ERROR_MISSING_DEFINITION}`
+					? `${componentPath} matches a pattern defined in “components” or “sections” options in your ` +
+					  'style guide config but doesn’t export a component.\n\n' +
+					  'It usually happens when using third-party libraries, see possible solutions here:\n' +
+					  `${consts.DOCS_THIRDPARTIES}`
+					: `Cannot parse ${componentPath}: ${err}\n\n` +
+					  'It usually means that react-docgen does not understand your source code, try to file an issue here:\n' +
+					  'https://github.com/reactjs/react-docgen/issues';
+			logger.warn(message);
+		}
 	}
 
 	const tempDocs = getProps(docs, file);
